72f3880b0d9061dc854ce33831ec436790263c2a
ThePurified <116533761+ThePurified@users.noreply.github.com>
2022-10-27 06:20:32 -0400

    Add files via upload

---
+++ Items/Potions/NotFabsolVodka.cs
@@ -0,0 +1,45 @@
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using Terraria.GameContent.Creative;
+using CalamityMod.Items.Potions.Alcohol;
+using CalamityMod.Items.Materials;
+
+namespace CalRemix.Items.Potions
+{
+    public class NotFabsolVodka : ModItem
+    {
+        public override string Texture => "CalamityMod/Items/Potions/Alcohol/FabsolsVodka";
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Fluorescent Vodka");
+            Tooltip.SetDefault("Boosts all damage stats by 8% but lowers defense by 10%\nIncreases immune time after being struck");
+        }
+
+
+        public override void SetDefaults()
+        {
+            Item.CloneDefaults(ModContent.ItemType<FabsolsVodka>());
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe().
+                AddIngredient(ItemID.Ale, 1).
+                AddIngredient(ItemID.UnicornHorn, 1).
+                AddIngredient<BloodOrb>(40).
+                AddTile(TileID.Bottles).
+                Register();
+            CreateRecipe().
+                AddIngredient(ItemID.Ale, 1).
+                AddIngredient(ItemID.PixieDust, 10).
+                AddIngredient(ItemID.CrystalShard, 5).
+                AddIngredient(ItemID.UnicornHorn, 1).
+                AddTile(TileID.Kegs).
+                Register();
+        }
+    }
+}
