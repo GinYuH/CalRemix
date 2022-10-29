30beae0bb1d3edb2079c336f89737149968bc51c
MisterGoodLuck <79240324+MisterGoodLuck@users.noreply.github.com>
2022-10-28 20:52:52 -0700

    Update CalRemixRecipes.cs

--- CalRemixRecipes.cs
+++ CalRemixRecipes.cs
@@ -12,15 +12,32 @@ namespace CalRemix
 {
     public class Recipes : ModSystem
     {
+        public override void AddRecipes() 
+        {
+			Recipe recipe = Recipe.Create(ModContent.ItemType<EffulgentFeather>(), 3);
+			recipe.AddIngredient(DesertFeather, 3)
+				.AddIngredient<LifeAlloy>()
+                .AddTile(TileID.LunarCraftingStation)
+				.Register();
+        }
         public override void PostAddRecipes()
         {
-            for (int Fab = 0; Fab < Recipe.numRecipes; Fab++)
+            for (int i = 0; i < Recipe.numRecipes; i++)
             {
-                Recipe recipe = Main.recipe[Fab];
+                Recipe recipe = Main.recipe[i];
+                
                 if (recipe.HasResult(ModContent.ItemType<FabsolsVodka>()))
                 {
                     recipe.DisableRecipe();
                 }
+                if (recipe.HasResult(ModContent.ItemType<GrandScale>()))
+                {
+                    recipe.DisableRecipe();
+                }
+                if (recipe.TryGetIngredient(ModContent.ItemType<PearlShard>()), out Item shard) && recipe.HasResult(ModContent.ItemType<SeaRemains>()) || recipe.HasResult(ModContent.ItemType<MonstrousKnives>()) || recipe.HasResult(ModContent.ItemType<FirestormCannon>()) || recipe.HasResult(ModContent.ItemType<SuperballBullet>()))
+				{
+					shard.type = ModContent.ItemType<ParchedScale>();
+				}
             }
         }
     }
