b2b0e9d5b89c86a105a6294449c3fcc911435482
GinYuH <>
2022-10-30 10:04:28 -0700

    Added Holy Mantle

---
+++ Items/Accessories/HolyMantle.cs
@@ -0,0 +1,68 @@
+﻿using CalamityMod.Rarities;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Graphics.Shaders;
+using Terraria.ID;
+using CalamityMod.Items.Accessories.Wings;
+using CalamityMod.Items.Materials;
+using CalamityMod.Items;
+using CalamityMod;
+using CalamityMod.Tiles.Furniture.CraftingStations;
+using Terraria.ModLoader;
+
+namespace CalRemix.Items.Accessories
+{
+    [AutoloadEquip(EquipType.Wings)]
+    public class HolyMantle : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            SacrificeTotal = 1;
+            DisplayName.SetDefault("Holy Mantle");
+            Tooltip.SetDefault("Delirious thoughts...\n"+
+            "Horizontal speed: 14\n"+
+            "Acceleration multiplier: 3.2\n"+
+                "Flight time: ∞");
+            ArmorIDs.Wing.Sets.Stats[Item.wingSlot] = new WingStats(500, 14f, 3.2f);
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 22;
+            Item.height = 20;
+            Item.value = CalamityGlobalItem.Rarity15BuyPrice;
+            Item.rare = ModContent.RarityType<Violet>();
+            Item.accessory = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            if (player.wingTime < 500)
+            {
+                player.wingTime = 500;
+            }
+            player.noFallDmg = true;
+        }
+
+        public override void VerticalWingSpeeds(Player player, ref float ascentWhenFalling, ref float ascentWhenRising, ref float maxCanAscendMultiplier, ref float maxAscentMultiplier, ref float constantAscend)
+        {
+            ascentWhenFalling = 1f; 
+            ascentWhenRising = 0.175f;
+            maxCanAscendMultiplier = 1.2f; 
+            maxAscentMultiplier = 3.25f; 
+            constantAscend = 0.15f;
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe().
+                AddIngredient<DrewsWings>(1).
+                AddIngredient(ItemID.EmpressFlightBooster).
+                AddIngredient<MOAB>(1).
+                AddIngredient(ItemID.SoulofFlight, 20).
+                AddIngredient<AshesofAnnihilation>(5).
+                AddTile<CosmicAnvil>().
+                Register();
+        }
+    }
+}
