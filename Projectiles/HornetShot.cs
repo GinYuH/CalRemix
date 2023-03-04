bafe02e779ad538ff6058265e157cb9b7c6d109f
GinYuH <>
2023-03-04 15:58:04 -0800

    reworked 22 hornet round

--- Projectiles/HornetShot.cs
+++ Projectiles/HornetShot.cs
@@ -2,11 +2,13 @@ using CalamityMod;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using System.Collections.Generic;
 
 namespace CalRemix.Projectiles
 {
 	public class HornetShot : ModProjectile
 	{
+        List<int> hit = new List<int>{ -1 };
         public override string Texture => "Terraria/Images/Projectile_242";
         public override void SetStaticDefaults() 
         {
@@ -15,12 +17,24 @@ namespace CalRemix.Projectiles
 		public override void SetDefaults() 
         {
             Projectile.CloneDefaults(ProjectileID.BulletHighVelocity);
-            Projectile.penetrate = 22;
             AIType = ProjectileID.BulletHighVelocity;
-		}
+            Projectile.tileCollide = false;
+            Projectile.penetrate = 22;
+            Projectile.timeLeft = 2222;
+        }
 		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
 		{
-            NPC npc = Projectile.Center.MinionHoming(1750f, Main.player[Projectile.owner], false);
+            hit.Add(target.whoAmI);
+            NPC npc = target;
+            for (int i = 0; i < Main.maxNPCs; i++)
+            {
+                NPC newTarget = Main.npc[i];
+                if (newTarget != npc && newTarget != null && !hit.Contains(newTarget.whoAmI) && newTarget.chaseable && !newTarget.friendly)
+                {
+                    npc = newTarget;
+                    break;
+                }
+            }
             if (npc != null && npc != target)
             {
                 Projectile.velocity = npc.velocity + Projectile.DirectionTo(npc.Center) * 22f;
