05c7fbbb7d8c8f330973cf66043cc766517f0c21
GinYuH <>
2023-06-30 20:21:38 -0700

    grarble (unfinished)

---
+++ Items/GrarbleObliterator.cs
@@ -0,0 +1,51 @@
+using CalRemix.Projectiles;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using CalamityMod;
+using CalamityMod.Items;
+namespace CalRemix.Items
+
+{
+    public class GrarbleObliterator : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Grarble Obliterator");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 13;
+            Item.knockBack = 1f;
+            Item.useTime = 6;
+            Item.useAnimation = 25;
+            Item.pick = 50;
+            Item.axe = 30 / 5;
+
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 36;
+            Item.height = 18;
+            Item.channel = true;
+            Item.noUseGraphic = true;
+            Item.noMelee = true;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.value = CalamityGlobalItem.Rarity1BuyPrice;
+            Item.rare = ItemRarityID.Blue;
+            Item.UseSound = SoundID.Item23;
+            Item.autoReuse = true;
+            Item.shoot = ModContent.ProjectileType<GrarbleObliteratorProj>();
+            Item.shootSpeed = 40f;
+		}
+
+        public override void AddRecipes()
+        {
+            CreateRecipe().
+                AddRecipeGroup("AnyGoldBar", 3).
+                AddIngredient(ItemID.Granite, 5).
+                AddIngredient(ItemID.Marble ,5).
+                AddTile(TileID.Anvils).
+                Register();
+        }
+    }
+}
---
+++ Projectiles/GrarbleObliteratorProj.cs
@@ -0,0 +1,26 @@
+using Terraria.ModLoader;
+using CalamityMod;
+
+namespace CalRemix.Projectiles
+{
+    public class GrarbleObliteratorProj : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Obliterator");
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 14;
+            Projectile.height = 14;
+            Projectile.aiStyle = 20;
+            Projectile.friendly = true;
+            Projectile.penetrate = -1;
+            Projectile.tileCollide = false;
+            Projectile.hide = true;
+            Projectile.ownerHitCheck = true;
+            Projectile.DamageType = Terraria.ModLoader.DamageClass.Melee;
+        }
+    }
+}
