aae8c3519fb3876b2cd7ab7545d6c1035c7f5e8e
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2023-12-06 19:59:15 +0200

    wulfrum leech dagger, coldheart icicle (#6)

---
+++ Gores/WulfrumLeechDaggerGores.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.GameContent;
+using Terraria.ModLoader;
+
+namespace CalRemix.Gores
+{
+    public class WulfrumLeechDaggerGore0 : ModGore
+    {
+        public override void SetStaticDefaults()
+        {
+            ChildSafety.SafeGore[Type] = true;
+        }
+    }
+    public class WulfrumLeechDaggerGore1 : ModGore {
+        public override void SetStaticDefaults()
+        {
+            ChildSafety.SafeGore[Type] = true;
+        }
+    }
+}
---
+++ Particles/Snowflake.cs
@@ -0,0 +1,62 @@
+﻿using CalamityMod.Particles;
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.Chat.Commands;
+using Terraria.GameContent.Biomes;
+
+namespace CalRemix.Particles
+{
+    public class Snowflake : Particle
+    {
+        public override bool SetLifetime => true;
+        public override bool UseHalfTransparency => true;
+        public override string Texture => "CalRemix/Particles/Snowflake";
+
+        public float VelocityDiminish { get; set; }
+        public float VanishPercentage { get; set; }
+        public float SpinAmount { get; set; }
+
+        private delegate double DoubleOutput(double input);
+        private DoubleOutput velocityMethod;
+
+        public Snowflake(Vector2 position, Vector2 velocity, int lifetime, float scale, Color color)
+        {
+            Position = position;
+            Velocity = velocity;
+            Scale = scale;
+            Lifetime = lifetime;
+            Color = color;
+
+            velocityMethod = Main.rand.Next(3) switch
+            {
+                1 => Math.Cos,
+                2 => Math.Log,
+                3 => Math.Tan,
+                _ => Math.Sin
+            };
+        }
+
+        public override void Update()
+        {
+            if (Time % 20 == 0)
+            {
+                velocityMethod = Main.rand.Next(3) switch
+                {
+                    1 => Math.Cos,
+                    2 => Math.Log, 
+                    3 => Math.Tan,
+                    _ => Math.Sin
+                };
+            }
+            float velocityAxis = (float)velocityMethod(Time * Math.PI);
+            velocityAxis *= Main.rand.NextBool() ? 1 : -1;
+            Vector2 finalVelocity = new(velocityAxis);
+            Velocity = Vector2.Lerp(Velocity, finalVelocity, VelocityDiminish);
+
+            Rotation += Velocity.ToRotation() * Velocity.X > 0 ? 0.01f : -0.01f;
+
+            Color.A =(byte)(Utils.GetLerpValue(1f, VanishPercentage, LifetimeCompletion, true)*255f);
+        }
+    }
+}
