436d2b16ad81fa5f3a02482fbf24810681099b94
GinYuH <72181395+GinYuH@users.noreply.github.com>
2024-07-28 02:27:32 -0700

    Origen Point

---
+++ Items/Weapons/OrigenPointProjectile.cs
@@ -0,0 +1,77 @@
+﻿using CalamityMod;
+using CalRemix.Items.Weapons;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalRemix.Projectiles.Weapons
+{
+    public class OrigenPointProjectile : ModProjectile
+    {
+        public override string Texture => "CalRemix/Items/Weapons/OrigenPoint";
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Point");
+        }
+        public override void SetDefaults()
+        {
+            Projectile.width = 44;
+            Projectile.height = 44;
+            Projectile.friendly = true;
+            Projectile.timeLeft = 600;
+            Projectile.tileCollide = false;
+            Projectile.scale = 0.05f;
+            Projectile.penetrate = 3;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = 60;
+        }
+
+
+        public override void AI()
+        {
+            CalamityUtils.HomeInOnNPC(Projectile, true, 1200, 0.2f, 0);
+            Color color = OrigenPoint.origenPalette[(int)Main.rand.Next(0, OrigenPoint.origenPalette.Count)];
+            Lighting.AddLight(Projectile.Center, color.R * 0.00392f, color.G * 0.00392f, color.B * 0.00392f);
+            if (Projectile.timeLeft > 90)
+            {
+                if (Projectile.scale < 1)
+                {
+                    Projectile.scale += 0.05f;
+                }
+                else
+                {
+                    Projectile.scale = 1;
+                }
+            }
+            else
+            {
+                if (Projectile.timeLeft == 90)
+                {
+                    SoundEngine.PlaySound(CalamityMod.CalPlayer.CalamityPlayer.DrownSound, Projectile.Center);
+                }
+                if (Projectile.scale >= 0)
+                {
+                    Projectile.scale -= 0.05f;
+                }
+                if (Projectile.scale <= 0)
+                {
+                    Projectile.Kill();
+                }
+            }
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Color color = OrigenPoint.origenPalette[(int)Main.rand.Next(0, OrigenPoint.origenPalette.Count)];
+            Main.EntitySpriteDraw(TextureAssets.Projectile[Type].Value, Projectile.Center - Main.screenPosition, null, color, 0, TextureAssets.Projectile[Type].Value.Size() / 2, Projectile.scale, Microsoft.Xna.Framework.Graphics.SpriteEffects.None, 0);
+            return false;
+        }
+    }
+}
