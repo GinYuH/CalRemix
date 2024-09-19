9ef45ed0fb490ed63a17efdec3e332ae67e72074
GardicLunatik <gedgewithedge@gmail.com>
2024-09-18 17:02:00 -0700

    organized other assets

---
+++ Assets/Gores/WulfrumLeechDaggerGores.cs
@@ -0,0 +1,19 @@
+﻿using Terraria.GameContent;
+using Terraria.ModLoader;
+
+namespace CalRemix.Assets.Gores
+{
+    public class WulfrumLeechDaggerGore0 : ModGore
+    {
+        public override void SetStaticDefaults()
+        {
+            ChildSafety.SafeGore[Type] = true;
+        }
+    }
+    public class WulfrumLeechDaggerGore1 : ModGore {
+        public override void SetStaticDefaults()
+        {
+            ChildSafety.SafeGore[Type] = true;
+        }
+    }
+}
