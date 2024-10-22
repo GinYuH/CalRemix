77b98860c5ab618213b23327a7cb7194728d42ea
TohruKobayashi <t0hruk0b4y4sh1@gmail.com>
2024-10-21 20:46:22 -0500

    my power

---
+++ Content/Items/Weapons/TotalAnnihilation.cs
@@ -0,0 +1,51 @@
+﻿using CalamityMod.Projectiles.Ranged;
+using CalRemix.Content.Projectiles;
+using CalRemix.Content.Projectiles.Weapons;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalRemix.Content.Items.Weapons
+{
+    // ExampleFlail and ExampleFlailProjectile show the minimum amount of code needed for a flail using the existing vanilla code and behavior. ExampleAdvancedFlail and ExampleAdvancedFlailProjectile need to be consulted if more advanced customization is desired, or if you want to learn more advanced modding techniques.
+    // ExampleFlail is a copy of the Sunfury flail weapon.
+    internal class TotalAnnihilation : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            // This line will make the damage shown in the tooltip twice the actual Item.damage. This multiplier is used to adjust for the dynamic damage capabilities of the projectile.
+            // When thrown directly at enemies, the flail projectile will deal double Item.damage, matching the tooltip, but deals normal damage in other modes.
+            ItemID.Sets.ToolTipDamageMultiplier[Type] = 2f;
+        }
+
+        public override void SetDefaults()
+        {
+            // These default values aside from Item.shoot match the Sunfury values, feel free to tweak them.
+            Item.useStyle = ItemUseStyleID.Shoot; // How you use the item (swinging, holding out, etc.)
+            Item.useAnimation = 45; // The item's use time in ticks (60 ticks == 1 second.)
+            Item.useTime = 45; // The item's use time in ticks (60 ticks == 1 second.)
+            Item.knockBack = 6.75f; // The knockback of your flail, this is dynamically adjusted in the projectile code.
+            Item.width = 58; // Hitbox width of the item.
+            Item.height = 52; // Hitbox height of the item.
+            Item.damage = 32; // The damage of your flail, this is dynamically adjusted in the projectile code.
+            Item.crit = 7; // Critical damage chance %
+            Item.scale = 1.1f; 
+            Item.noUseGraphic = true; // This makes sure the item does not get shown when the player swings his hand
+            Item.shoot = ModContent.ProjectileType<TotalAnnihilationProj>(); // The flail projectile
+            Item.shootSpeed = 22f; // The speed of the projectile measured in pixels per frame.
+            Item.UseSound = SoundID.Item1; // The sound that this item makes when used
+            Item.rare = ItemRarityID.Orange; // The color of the name of your item
+            Item.value = Item.sellPrice(gold: 2, silver: 50); // Sells for 2 gold 50 silver
+            Item.DamageType = DamageClass.MeleeNoSpeed; // Deals melee damage
+            Item.channel = true;
+            Item.noMelee = true; // This makes sure the item does not deal damage from the swinging animation
+        }
+
+        public override Color? GetAlpha(Color lightColor)
+        {
+            // Aside from SetDefaults, when making a copy of a vanilla weapon you may have to hunt down other bits of code. This code makes the item draw in full brightness when dropped.
+            return Color.White;
+        }
+    }
+}
---
+++ Content/Items/Weapons/TotalAnnihilationProjOrbital.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Mono.Cecil;
+using ReLogic.Content;
+using System;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalRemix.Content.Projectiles.Weapons
+{
+    // Example Advanced Flail is a complete adaption of Ball O' Hurt projectile. The code has been rewritten a bit to make it easier to follow. Compare this code against the decompiled Terraria code for an example of adapting vanilla code. A few comments and extra code snippets show features from other vanilla flails as well.
+    // Example Advanced Flail shows a plethora of advanced AI and collision topics.
+    // See ExampleFlail for a simpler but less customizable flail projectile example.
+    public class TotalAnnihilationProjOrbital : ModProjectile
+    {
+        private const string ChainTexturePath = "CalRemix/Content/Projectiles/Weapons/TotalAnnihilationChain"; // The folder path to the flail chain sprite
+        private const string ChainTextureExtraPath = "CalRemix/Content/Projectiles/Weapons/TotalAnnihilationChain2";  // This texture and related code is optional and used for a unique effect
+
+        private static Asset<Texture2D> chainTexture;
+
+        public ref float ParentIndex => ref Projectile.ai[0];
+        public ref float SpeedModifier => ref Projectile.localAI[2];
+
+        public override void Load()
+        {
+            chainTexture = ModContent.Request<Texture2D>(ChainTexturePath);
+        }
+
+        public override void OnSpawn(IEntitySource source)
+        {
+            Projectile.ai[2] = 0;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.netImportant = true; // This ensures that the projectile is synced when other players join the world.
+            Projectile.width = 44; // The width of your projectile
+            Projectile.height = 40; // The height of your projectile
+            Projectile.friendly = true; // Deals damage to enemies
+            Projectile.penetrate = -1; // Infinite pierce
+            Projectile.DamageType = DamageClass.Melee; // Deals melee damage
+            Projectile.usesLocalNPCImmunity = true; // Used for hit cooldown changes in the ai hook
+            Projectile.localNPCHitCooldown = 10; // This facilitates custom hit cooldown logic
+            Projectile.tileCollide = false;
+
+            Main.projFrames[ModContent.ProjectileType<TotalAnnihilationProjOrbital>()] = 5;
+
+            // Vanilla flails all use aiStyle 15, but the code isn't customizable so an adaption of that aiStyle is used in the AI method
+        }
+
+        // This AI code was adapted from vanilla code: Terraria.Projectile.AI_015_Flails() 
+        public override void AI()
+        {
+            // onspawn is one of the seven wonders of the world so we cant use it for this
+            if (Projectile.ai[2] == 0)
+            {
+                switch (Projectile.frame)
+                {
+                    case 0: // melee
+                        int ranged = Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Vector2.Zero, ModContent.ProjectileType<TotalAnnihilationProjOrbital>(), 0, 0, -1, Projectile.whoAmI);
+                        Main.projectile[ranged].frame = 1;
+                        Main.projectile[ranged].localAI[2] = 0.6f;
+                        break;
+                    case 1: // ranged
+                        int mage = Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Vector2.Zero, ModContent.ProjectileType<TotalAnnihilationProjOrbital>(), 0, 0, -1, Projectile.whoAmI);
+                        Main.projectile[mage].frame = 2;
+                        Main.projectile[mage].localAI[2] = 1.2f;
+                        break;
+                    case 2: // mage
+                        int summoner = Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Vector2.Zero, ModContent.ProjectileType<TotalAnnihilationProjOrbital>(), 0, 0, -1, Projectile.whoAmI);
+                        Main.projectile[summoner].frame = 3;
+                        Main.projectile[summoner].localAI[2] = 0.75f;
+                        break;
+                    case 3: // summoner
+                        int rogue = Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Vector2.Zero, ModContent.ProjectileType<TotalAnnihilationProjOrbital>(), 0, 0, -1, Projectile.whoAmI);
+                        Main.projectile[rogue].frame = 4;
+                        Main.projectile[rogue].localAI[2] = 1.4f;
+                        break;
+                    case 4: // dogshit
+                        // do nothing bcuz STUPID
+                        break;
+                }
+                Projectile.ai[2] = 1;
+            }
+            
+            Projectile parent = Main.projectile[(int)ParentIndex];
+            // Kill
+            if (!parent.active)
+            {
+                Projectile.Kill();
+                return;
+            }
+
+            // the following code is ripped from goozmaga to give this item maximum spiritual dogshit strength
+            Projectile.velocity = Vector2.Zero;
+            Projectile.localAI[1]++;
+            float distance = 50;
+            double deg = Main.GlobalTimeWrappedHourly * 360 * (1 + Math.Clamp(Projectile.localAI[0], 0, 0.5f)) + Projectile.localAI[1] * SpeedModifier;
+            double rad = deg * (Math.PI / 180);
+            float hyposx = parent.Center.X - (int)(Math.Cos(rad) * distance) - Projectile.width / 2;
+            float hyposy = parent.Center.Y - (int)(Math.Sin(rad) * distance) - Projectile.height / 2;
+
+            Projectile.position = new Vector2(hyposx, hyposy);
+        }
+
+        // PreDraw is used to draw a chain and trail before the projectile is drawn normally.
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Projectile parent = Main.projectile[(int)ParentIndex];
+            Vector2 playerArmPosition = parent.Center;
+
+            Rectangle? chainSourceRectangle = null;
+            // Drippler Crippler customizes sourceRectangle to cycle through sprite frames: sourceRectangle = asset.Frame(1, 6);
+            float chainHeightAdjustment = 0f; // Use this to adjust the chain overlap. 
+
+            Vector2 chainOrigin = chainSourceRectangle.HasValue ? (chainSourceRectangle.Value.Size() / 2f) : (chainTexture.Size() / 2f);
+            Vector2 chainDrawPosition = Projectile.Center;
+            Vector2 vectorFromProjectileToPlayerArms = playerArmPosition.MoveTowards(chainDrawPosition, 4f) - chainDrawPosition;
+            Vector2 unitVectorFromProjectileToPlayerArms = vectorFromProjectileToPlayerArms.SafeNormalize(Vector2.Zero);
+            float chainSegmentLength = (chainSourceRectangle.HasValue ? chainSourceRectangle.Value.Height : chainTexture.Height()) + chainHeightAdjustment;
+            if (chainSegmentLength == 0)
+            {
+                chainSegmentLength = 10; // When the chain texture is being loaded, the height is 0 which would cause infinite loops.
+            }
+            float chainRotation = unitVectorFromProjectileToPlayerArms.ToRotation() + MathHelper.PiOver2;
+            int chainCount = 0;
+            float chainLengthRemainingToDraw = vectorFromProjectileToPlayerArms.Length() + chainSegmentLength / 2f;
+
+            // This while loop draws the chain texture from the projectile to the player, looping to draw the chain texture along the path
+            while (chainLengthRemainingToDraw > 0f)
+            {
+                // This code gets the lighting at the current tile coordinates
+                Color chainDrawColor = Lighting.GetColor((int)chainDrawPosition.X / 16, (int)(chainDrawPosition.Y / 16f));
+
+                // Flaming Mace and Drippler Crippler use code here to draw custom sprite frames with custom lighting.
+                // Cycling through frames: sourceRectangle = asset.Frame(1, 6, 0, chainCount % 6);
+                // This example shows how Flaming Mace works. It checks chainCount and changes chainTexture and draw color at different values
+
+                var chainTextureToDraw = chainTexture;
+
+                // Here, we draw the chain texture at the coordinates
+                Main.spriteBatch.Draw(chainTextureToDraw.Value, chainDrawPosition - Main.screenPosition, chainSourceRectangle, chainDrawColor, chainRotation, chainOrigin, 1f, SpriteEffects.None, 0f);
+
+                // chainDrawPosition is advanced along the vector back to the player by the chainSegmentLength
+                chainDrawPosition += unitVectorFromProjectileToPlayerArms * chainSegmentLength;
+                chainCount++;
+                chainLengthRemainingToDraw -= chainSegmentLength;
+            }
+            return true;
+        }
+    }
+}
