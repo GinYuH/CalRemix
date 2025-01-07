c61876a3a608b0255a4af83c8153a9f8d3fcc3bd
TohruKobayashi <t0hruk0b4y4sh1@gmail.com>
2025-01-07 00:53:53 -0600

    chlorium like 4 or whatever

---
+++ Content/Items/Weapons/ChloriumPike.cs
@@ -0,0 +1,36 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using CalamityMod.Items;
+using CalRemix.Content.Projectiles.Weapons;
+using CalRemix.Content.Items.Materials;
+
+namespace CalRemix.Content.Items.Weapons
+{
+	public class ChloriumPike : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            ItemID.Sets.Spears[Item.type] = true;
+        }
+        public override void SetDefaults() 
+		{
+            Item.CloneDefaults(ItemID.AdamantiteGlaive);
+            Item.rare = ItemRarityID.Lime;
+        }
+        public override bool CanUseItem(Player player)
+        {
+            // Ensures no more than one spear can be thrown out, use this when using autoReuse
+            return player.ownedProjectileCounts[Item.shoot] < 1;
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe().
+                AddIngredient<ChloriumBar>(12).
+                AddTile(TileID.MythrilAnvil).
+                Register();
+        }
+    }
+}
