17deb0d0f2c65770a6f73fdcabbf4d798529f1e5
TohruKobayashi <t0hruk0b4y4sh1@gmail.com>
2025-01-06 21:43:57 -0600

    chlorium 1

---
+++ Content/Items/Tools/Chlorium/ChloriumChainsaw.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using CalRemix.Content.Projectiles;
+using CalRemix.Content.Items.Materials;
+
+namespace CalRemix.Content.Items.Tools.Chlorium
+{
+    public class ChloriumChainsaw : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            // As mentioned in the documentation, IsDrill and IsChainsaw automatically reduce useTime and useAnimation to 60% of what is set in SetDefaults and decrease tileBoost by 1, but only for vanilla items.
+            // We set it here despite it doing nothing because it is likely to be used by other mods to provide special effects to drill or chainsaw items globally.
+            ItemID.Sets.IsChainsaw[Type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.CloneDefaults(ItemID.ChlorophyteChainsaw);
+            Item.shoot = ModContent.ProjectileType<ChloriumChainsawProjectile>();
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe().
+                AddIngredient<ChloriumBar>(12).
+                AddTile(TileID.MythrilAnvil).
+                Register();
+        }
+    }
+}
---
+++ Content/Items/Tools/Chlorium/ChloriumPaintbrush.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using CalRemix.Content.Projectiles;
+using CalRemix.Content.Items.Materials;
+
+namespace CalRemix.Content.Items.Tools.Chlorium
+{
+    public class ChloriumPaintbrush : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.CloneDefaults(ItemID.Paintbrush);
+            Item.tileBoost = 1;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe().
+                AddIngredient<ChloriumBar>(8).
+                AddTile(TileID.MythrilAnvil).
+                Register();
+        }
+    }
+}
