6d619160d32fd1dae882f07ddb9c0ebcd645f5b9
GardicLunatik <gedgewithedge@gmail.com>
2025-01-08 21:28:49 -0800

    fixed wrong check

--- Content/Tiles/Relics/CalamityRelicPlaced.cs
+++ Content/Tiles/Relics/CalamityRelicPlaced.cs
@@ -6,7 +6,7 @@ namespace CalRemix.Content.Tiles.Relics
 {
     public class CalamityRelicPlaced : BaseBossRelic
     {
-        public override string RelicTextureName => "CalRemix/Content/Tiles/Relics/CalamityRelicPlaced" + (Main.zenithWorld ? string.Empty : "2");
+        public override string RelicTextureName => "CalRemix/Content/Tiles/Relics/CalamityRelicPlaced" + (!Main.zenithWorld ? string.Empty : "2");
         public override int AssociatedItem => ModContent.ItemType<Items.Placeables.Relics.CalamityRelic>();
     }
 }
