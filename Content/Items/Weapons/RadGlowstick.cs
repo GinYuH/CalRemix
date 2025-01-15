a31564ad777c1c120a6bad3ef2f4fc2fd80878a9
TohruKobayashi <t0hruk0b4y4sh1@gmail.com>
2025-01-15 03:46:20 -0600

    rad glowstick

---
+++ Content/Items/Weapons/RadGlowstick.cs
@@ -0,0 +1,29 @@
+﻿using CalamityMod.Rarities;
+using CalamityMod.Items;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using CalamityMod;
+using CalamityMod.CalPlayer;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
+using CalRemix.Content.Projectiles.Weapons;
+using CalamityMod.Items.Weapons.Rogue;
+
+namespace CalRemix.Content.Items.Weapons
+{
+    public class RadGlowstick : RogueWeapon
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 1;
+        }
+        public override void SetDefaults()
+        {
+            Item.CloneDefaults(ItemID.Glowstick);
+            Item.damage = 23;
+            Item.shoot = ModContent.ProjectileType<RadGlowstickProjectile>();
+            Item.DamageType = ModContent.GetInstance<RogueDamageClass>();
+        }
+    }
+}
---
+++ Content/Projectiles/Weapons/RadGlowstickProjectile.cs
@@ -0,0 +1,23 @@
+﻿using CalamityMod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalRemix.Content.Projectiles.Weapons
+{
+    public class RadGlowstickProjectile : ModProjectile
+    {
+        public override string Texture => "CalRemix/Content/Items/Weapons/RadGlowstick";
+        public override void SetDefaults()
+        {
+            Projectile.CloneDefaults(ProjectileID.Glowstick);
+            AIType = ProjectileID.Glowstick;
+            Projectile.damage = 23;
+            Projectile.DamageType = ModContent.GetInstance<RogueDamageClass>();
+        }
+    }
+}
