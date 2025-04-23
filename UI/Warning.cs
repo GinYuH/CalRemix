70518ab94d1a5c7f4235467b79331101c78d9348
GardicLunatik <gedgewithedge@gmail.com>
2025-04-22 17:43:00 -0700

    made warning powerful (stop using fandom)

--- UI/Warning.cs
+++ UI/Warning.cs
@@ -1,70 +1,39 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
-using System.Collections.Generic;
 using Terraria;
 using Terraria.GameContent;
 using Terraria.ModLoader;
-using Terraria.UI;
 
 namespace CalRemix.UI
 {
-    public class WarningUI : UIState
-    {
-        public override void Update(GameTime gameTime)
-        {
-            base.Update(gameTime);
-        }
-        public override void Draw(SpriteBatch spriteBatch)
-        {
-            if (Main.gameMenu)
-                return;
-            if (Main.LocalPlayer is null)
-                return;
-            if (!Main.LocalPlayer.TryGetModPlayer(out CalRemixPlayer p) || p.onFandom <= 0)
-                return;
-            base.Draw(spriteBatch);
-        }
-        protected override void DrawSelf(SpriteBatch spriteBatch)
-        {
-            string nofandom = CalRemixHelper.LocalText("UI.FandomWarning").Value;
-            float fandomwidth = FontAssets.MouseText.Value.MeasureString(nofandom).X;
-            float fandomheight = FontAssets.MouseText.Value.MeasureString(nofandom).Y;
-            float scale = (Main.screenWidth / fandomwidth) * 0.75f + (float)Math.Cos(Main.GlobalTimeWrappedHourly * 22) * 0.1f;
-            float scaledWidth = fandomwidth * scale;
-            float scaledHeight = fandomheight * scale;
-            Utils.DrawBorderString(spriteBatch, nofandom, new Vector2((Main.screenWidth - scaledWidth) / 2, (Main.screenHeight - scaledHeight) / 2) + Main.rand.NextVector2Square(-10, 10), Color.Red, scale);
-            spriteBatch.Draw(TextureAssets.MagicPixel.Value, new Rectangle(0, 0, Main.screenWidth * 4, Main.screenHeight * 4), null, Color.Red * 0.22f, 0f, TextureAssets.MagicPixel.Value.Size() * 0.5f, 0, 0f);
-        }
-    }
     [Autoload(Side = ModSide.Client)]
     public class Warning : ModSystem
     {
-        private UserInterface WarningUserInterface;
-        internal WarningUI WarningState;
         public override void Load()
         {
-            WarningState = new();
-            WarningUserInterface = new();
-            WarningUserInterface.SetState(WarningState);
+            On_Main.DoDraw += DrawWarning;
         }
-        public override void UpdateUI(GameTime gameTime)
+        private static void DrawWarning(On_Main.orig_DoDraw orig, Main self, GameTime gameTime)
         {
-            WarningUserInterface?.Update(gameTime);
-        }
-        public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
-        {
-            int layerIndex = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Mouse Over"));
-            if (layerIndex != -1)
+            orig(self, gameTime);
+            SpriteBatch spriteBatch = Main.spriteBatch;
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, null, null, null, null, Main.UIScaleMatrix);
+            if (!Main.gameMenu || Main.LocalPlayer != null)
             {
-                layers.Insert(layerIndex, new LegacyGameInterfaceLayer("CalRemix: FandomWarning",
-                    delegate
-                    {
-                        WarningUserInterface.Draw(Main.spriteBatch, new GameTime());
-                        return true;
-                    },
-                    InterfaceScaleType.UI));
+                if (Main.LocalPlayer.TryGetModPlayer(out CalRemixPlayer p) && p.onFandom > 0)
+                {
+                    string nofandom = CalRemixHelper.LocalText("UI.FandomWarning").Value;
+                    float fandomwidth = FontAssets.MouseText.Value.MeasureString(nofandom).X;
+                    float fandomheight = FontAssets.MouseText.Value.MeasureString(nofandom).Y;
+                    float scale = (Main.screenWidth / fandomwidth) * 0.75f + (float)Math.Cos(Main.GlobalTimeWrappedHourly * 22) * 0.1f;
+                    float scaledWidth = fandomwidth * scale;
+                    float scaledHeight = fandomheight * scale;
+                    Utils.DrawBorderString(spriteBatch, nofandom, new Vector2((Main.screenWidth - scaledWidth) / 2, (Main.screenHeight - scaledHeight) / 2) + Main.rand.NextVector2Square(-10, 10), Color.Red, scale);
+                    spriteBatch.Draw(TextureAssets.MagicPixel.Value, new Rectangle(0, 0, Main.screenWidth * 4, Main.screenHeight * 4), null, Color.Red * 0.22f, 0f, TextureAssets.MagicPixel.Value.Size() * 0.5f, 0, 0f);
+                }
             }
+            spriteBatch.End();
         }
         public static void CheckOnFandom(Player player)
         {
@@ -90,7 +59,8 @@ namespace CalRemix.UI
             {
                 Console.WriteLine(e.ToString());
             }
-            remixPlayer.onFandom = (fandomFound) ? 300 : -1;
+            if (fandomFound)
+                remixPlayer.onFandom = 300;
         }
     }
 }
