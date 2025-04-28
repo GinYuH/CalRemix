0221d369388d294c77e17f4b9a21ba45f0a3e34b
TohruKobayashi <t0hruk0b4y4sh1@gmail.com>
2025-04-28 16:04:53 -0500

    important fix

--- UI/Fanny/MiracleBoy.Misc.cs
+++ UI/Fanny/MiracleBoy.Misc.cs
@@ -102,7 +102,7 @@ namespace CalRemix.UI
                 "MiracleBoyIdle", (ScreenHelperSceneMetrics metrics) => Main.mouseRight && Main.mouseRightRelease && Main.rand.NextBool(100)).SpokenByAnotherHelper(ScreenHelpersUIState.MiracleBoy).SetHoverTextOverride("May God save us all");
 
             HelperMessage.New("Loser", "Loser",
-                "MiracleBoyIdle", (ScreenHelperSceneMetrics scene) => Main.LocalPlayer.HasBuff(BuffID.PotionSickness), cooldown: 55, onlyPlayOnce: false, cantBeClickedOff: true, duration: 55).SpokenByAnotherHelper(ScreenHelpersUIState.Flux);
+                "MiracleBoyIdle", (ScreenHelperSceneMetrics scene) => Main.LocalPlayer.HasBuff(BuffID.PotionSickness), cooldown: 55, onlyPlayOnce: false, cantBeClickedOff: true, duration: 55).SpokenByAnotherHelper(ScreenHelpersUIState.MiracleBoy);
 
 
         }
