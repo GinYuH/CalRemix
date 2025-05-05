7c5df249650374a640361b6b9e23b184740d2d7b
GinYuH <72181395+GinYuH@users.noreply.github.com>
2025-05-04 22:37:51 -0700

    door

---
+++ Content/Items/Placeables/SubworldDoor.cs
@@ -0,0 +1,28 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using CalRemix.Content.Tiles;
+using CalRemix.Content.Items.Placeables.Plates;
+
+namespace CalRemix.Content.Items.Placeables
+{
+    public class SubworldDoor : ModItem
+    {
+        public override string Texture => "CalRemix/Assets/ExtraTextures/SludgeCannon";
+
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 32;
+            Item.maxStack = 99;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.consumable = true;
+            Item.rare = ItemRarityID.Blue;
+            Item.createTile = ModContent.TileType<ExosphereDoor>();
+        }
+    }
+}
---
+++ Content/Tiles/SubworldDoorPlaced.cs
@@ -0,0 +1,130 @@
+﻿using CalamityMod;
+using CalamityMod.CalPlayer;
+using CalamityMod.Cooldowns;
+using CalamityMod.Items.Accessories;
+using CalRemix.Content.NPCs.Bosses.Origen;
+using CalRemix.Core.Subworlds;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using SubworldLibrary;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace CalRemix.Content.Tiles
+{
+    public abstract class SubworldDoorPlaced : ModTile
+    {
+        public static Asset<Texture2D> PreviewTex;
+
+        public virtual string PreviewTexName => null;
+        public virtual Subworld BoundSubworld => null;
+
+        public override void Load()
+        {
+            if (!Main.dedServ)
+            {
+                // Cache the extra texture displayed on the pedestal
+                PreviewTex = ModContent.Request<Texture2D>(PreviewTexName);
+            }
+        }
+
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2xX);
+            TileObjectData.newTile.Height = 3;
+            TileObjectData.newTile.Origin = new Point16(1, 2);
+            TileObjectData.newTile.CoordinateHeights = new[] { 16, 16, 16 };
+
+            TileID.Sets.PreventsTileRemovalIfOnTopOfIt[Type] = true;
+            TileID.Sets.PreventsTileReplaceIfOnTopOfIt[Type] = true;
+            TileID.Sets.PreventsSandfall[Type] = true;
+            TileObjectData.addTile(Type);
+            AddMapEntry(new Color(75, 139, 166));
+            DustType = 1;
+            AnimationFrameHeight = 54;
+            TileID.Sets.DisableSmartCursor[Type] = true;
+        }
+        public override bool CanKillTile(int i, int j, ref bool blockDamaged)
+        {
+            return false;
+        }
+
+        public override bool CanExplode(int i, int j)
+        {
+            return false;
+        }
+
+        public override bool RightClick(int i, int j)
+        {
+            if (Main.tile[i, j].TileFrameY >= AnimationFrameHeight)
+            {
+                SubworldSystem.Enter(BoundSubworld.FullName);
+                SoundEngine.PlaySound(BetterSoundID.ItemTeleportMirror);
+            }
+            else
+            {
+                HitWire(i, j);
+            }
+            return true;
+        }
+
+        public override void HitWire(int i, int j)
+        {
+            int x = i - Main.tile[i, j].TileFrameX / 18 % 2;
+            int y = j - Main.tile[i, j].TileFrameY / 18 % 3;
+            for (int l = x; l < x + 3; l++)
+            {
+                for (int m = y; m < y + 3; m++)
+                {
+                    if (Main.tile[l, m].HasTile && Main.tile[l, m].TileType == Type)
+                    {
+                        if (Main.tile[l, m].TileFrameY < 54)
+                        {
+                            Main.tile[l, m].TileFrameY += 54;
+                        }
+                        else
+                        {
+                            Main.tile[l, m].TileFrameY -= 54;
+                        }
+                    }
+                }
+            }
+            if (Wiring.running)
+            {
+                Wiring.SkipWire(x, y);
+                Wiring.SkipWire(x, y + 1);
+                Wiring.SkipWire(x, y + 2);
+                Wiring.SkipWire(x + 1, y);
+                Wiring.SkipWire(x + 1, y + 1);
+                Wiring.SkipWire(x + 1, y + 2);
+            }
+            NetMessage.SendTileSquare(-1, x, y + 1, 3);
+        }
+
+        public override void PostDraw(int i, int j, SpriteBatch spriteBatch)
+        {
+            Tile t = Main.tile[i, j];
+            if (t.TileFrameX == 18 && t.TileFrameY == 90)
+            {
+                Texture2D tex = PreviewTex.Value;
+                Vector2 tileSize = new Vector2(32, 54);
+                Main.EntitySpriteDraw(tex, new Vector2(i - 1, j - 2) * 16 - Main.screenPosition + CalamityUtils.TileDrawOffset, null, Color.White, 0, Vector2.Zero, tileSize / tex.Size(), 0);
+            }
+        }
+    }
+
+    public class ExosphereDoor : SubworldDoorPlaced
+    {
+        public override string PreviewTexName => "CalRemix/Assets/ExtraTextures/SubworldPreviews/ExospherePreview";
+        public override Subworld BoundSubworld => ModContent.GetInstance<ExosphereSubworld>();
+
+        public override string Texture => "CalRemix/Content/Tiles/SubworldDoorPlaced";
+    }
+}
