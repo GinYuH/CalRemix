b8a706e7a373ea994f4743bdc20fd1c9ca67b5a1
Sploopo <69737659+Sploopo@users.noreply.github.com>
2025-05-08 00:36:10 -0400

    random initial shit (no actual stocks yet)

---
+++ Content/Items/Misc/TraderTablet.cs
@@ -0,0 +1,31 @@
+﻿using CalamityMod.Items;
+using CalamityMod.NPCs.NormalNPCs;
+using CalamityMod.CalPlayer;
+using CalamityMod.NPCs.SlimeGod;
+using CalamityMod.Items.Materials;
+using CalRemix.Content.Projectiles.Accessories;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using static Terraria.ModLoader.ModContent;
+using Terraria.ModLoader;
+
+namespace CalRemix.Content.Items.Misc
+{
+    public class TraderTablet : ModItem
+    {
+        public override string Texture => "CalRemix/icon_small";
+
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 1;
+        }
+        public override void SetDefaults()
+        {
+            Item.width = 30;
+            Item.height = 30;
+            Item.value = 0;
+            Item.rare = ItemRarityID.LightRed;
+        }
+    }
+}
