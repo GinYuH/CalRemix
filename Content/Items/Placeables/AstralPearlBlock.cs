17471c86956990814c3cddcf615d67b05939f892
GinYuH <72181395+GinYuH@users.noreply.github.com>
2025-06-19 12:50:41 -0700

    Astral Pearl and Eye blocks

---
+++ Content/Items/Placeables/AstralPearlBlock.cs
@@ -0,0 +1,39 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using CalRemix.Content.Tiles;
+using CalRemix.Core.World;
+using CalRemix.Content.Items.Accessories;
+
+namespace CalRemix.Content.Items.Placeables
+{
+    public class AstralPearlBlock : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 100;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTurn = true;
+            Item.useAnimation = 14;
+            Item.useTime = 10;
+            Item.autoReuse = true;
+            Item.maxStack = 9999;
+            Item.consumable = true;
+            Item.createTile = ModContent.TileType<AstralPearlBlockPlaced>();
+            Item.width = 12;
+            Item.height = 12;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe().
+                AddIngredient(ModContent.ItemType<AstralPearl>(), 9).
+                AddTile(TileID.Anvils).
+                Register();
+        }
+    }
+}
---
+++ Content/Tiles/AstralPearlBlockPlaced.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace CalRemix.Content.Tiles
+{
+    public class AstralPearlBlockPlaced : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = false;
+            Main.tileBlockLight[Type] = true;
+            AddMapEntry(new Color(76, 137, 166));
+        }
+    }
+}
