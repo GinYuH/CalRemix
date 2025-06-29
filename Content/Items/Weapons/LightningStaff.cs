bc90e578bbbf67f6edd0e10b77c5260d8bf7ef4d
GinYuH <72181395+GinYuH@users.noreply.github.com>
2025-06-28 21:42:00 -0700

    Lightning Staff

---
+++ Content/Items/Weapons/LightningStaff.cs
@@ -0,0 +1,53 @@
+﻿using CalamityMod.Items;
+using CalamityMod.Items.Weapons.Magic;
+using CalamityMod.Rarities;
+using CalRemix.Content.Items.Materials;
+using CalRemix.Content.Projectiles.Weapons;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalRemix.Content.Items.Weapons;
+
+public class LightningStaff : ModItem
+{
+
+    public override void SetStaticDefaults()
+    {
+        Item.staff[Type] = true;
+    }
+
+    public override void SetDefaults()
+    {
+        Item.damage = 125;
+        Item.DamageType = DamageClass.Magic;
+        Item.width = 10;
+        Item.height = 10;
+        Item.useTime = 23;
+        Item.useAnimation = 23;
+        Item.mana = 3;
+        Item.useStyle = ItemUseStyleID.Shoot;
+        Item.noMelee = true;
+        Item.knockBack = 0;
+        Item.rare = ModContent.RarityType<Turquoise>();
+        Item.value = CalamityGlobalItem.RarityTurquoiseBuyPrice;
+        Item.UseSound = BetterSoundID.ItemMagicStaff;
+        Item.autoReuse = true;
+        Item.shoot = ModContent.ProjectileType<LightningBolt>();
+        Item.shootSpeed = 14f;
+    }
+
+    public override void AddRecipes()
+    {
+        CreateRecipe().
+            AddIngredient(ItemID.RubyStaff).
+            AddIngredient<KrakenTooth>().
+            AddIngredient(ItemID.Diamond).
+            AddTile(TileID.MythrilAnvil).
+            Register();
+    }
+
+}
+
---
+++ Content/Projectiles/Weapons/LightningBolt.cs
@@ -0,0 +1,59 @@
+﻿using CalamityMod.Projectiles.Rogue;
+using CalRemix.Content.Projectiles.Hostile;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalRemix.Content.Projectiles.Weapons
+{
+    public class LightningBolt : ModProjectile
+    {
+        public override string Texture => "CalamityMod/Projectiles/InvisibleProj";
+        public override void SetDefaults()
+        {
+            Projectile.width = 20;
+            Projectile.height = 20;
+            Projectile.friendly = true;
+            Projectile.timeLeft = 300;
+            Projectile.penetrate = 1;
+            Projectile.DamageType = DamageClass.Magic;
+        }
+        public override void AI()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                int dust = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.SpectreStaff, Projectile.velocity.X, Projectile.velocity.Y, Alpha: 255, Scale: Main.rand.NextFloat(1, 2));
+                Main.dust[dust].noGravity = true;
+            }
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            for (int i = 0; i < 80; i++)
+            {
+                Dust dust = Dust.NewDustPerfect(Projectile.Center, DustID.SpectreStaff, new Vector2(Projectile.velocity.X, Projectile.velocity.Y), Alpha: 255, Scale: Main.rand.NextFloat(2, 4));
+                dust.noGravity = true;
+                dust.position += Main.rand.NextVector2Square(-5, 5);
+                dust.velocity = Projectile.Center.DirectionTo(dust.position) * Main.rand.Next(3, 24);
+            }
+            SoundEngine.PlaySound(SoundID.Thunder, Projectile.Center);
+            for (int i = 0; i < 3; i++)
+            {
+                int lightningDamage = (int)(Projectile.damage * 5);
+                Vector2 lightningSpawnPosition = Projectile.Center - Vector2.UnitY.RotatedByRandom(0.2f) * 1000f;
+                Vector2 lightningShootVelocity = (Projectile.Center - lightningSpawnPosition + Projectile.velocity * 7.5f).SafeNormalize(Vector2.UnitY) * 15f;
+                int lightning = Projectile.NewProjectile(Projectile.GetSource_FromThis(), lightningSpawnPosition, lightningShootVelocity, ModContent.ProjectileType<StormfrontLightning>(), lightningDamage, 0f, Projectile.owner);
+                if (Main.projectile.IndexInRange(lightning))
+                {
+                    Main.projectile[lightning].CritChance = Projectile.CritChance;
+                    Main.projectile[lightning].ai[0] = lightningShootVelocity.ToRotation();
+                    Main.projectile[lightning].ai[1] = Main.rand.Next(100);
+                    Main.projectile[lightning].DamageType = DamageClass.Magic;
+                    Main.projectile[lightning].tileCollide = false;
+                }
+            }
+        }
+    }
+}
