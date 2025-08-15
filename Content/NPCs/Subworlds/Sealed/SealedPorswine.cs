68a1ee6e71a392f9d3c0683e6170b610a6e39836
GinYuH <72181395+GinYuH@users.noreply.github.com>
2025-08-14 21:42:15 -0700

    Added Sealed Porswine

---
+++ Content/NPCs/Subworlds/Sealed/SealedPorswine.cs
@@ -0,0 +1,82 @@
+﻿using CalamityMod;
+using CalRemix.Core.Biomes;
+using Terraria;
+using Terraria.GameContent.Bestiary;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Utilities;
+using static Terraria.ModLoader.ModContent;
+
+namespace CalRemix.Content.NPCs.Subworlds.Sealed
+{
+    public class SealedPorswine : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.npcFrameCount[NPC.type] = 6;
+            Main.npcCatchable[NPC.type] = true;
+            NPCID.Sets.CantTakeLunchMoney[Type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            NPC.width = 50;
+            NPC.height = 24;
+            NPC.CloneDefaults(NPCID.Penguin);
+            NPC.lavaImmune = false;
+            AIType = NPCID.Penguin;
+            NPC.HitSound = SoundID.NPCHit1;
+            NPC.DeathSound = SoundID.NPCDeath1;
+            NPC.lifeMax = 200;
+            NPC.defense = 0;
+            NPC.chaseable = false;
+            NPC.Calamity().VulnerableToHeat = true;
+            NPC.Calamity().VulnerableToSickness = false;
+            NPC.Calamity().VulnerableToCold = true;
+            SpawnModBiomes = [ ModContent.GetInstance<SealedFieldsBiome>().Type, ModContent.GetInstance<TurnipBiome>().Type ];
+        }
+
+        public override void AI()
+        {
+            NPC.spriteDirection = NPC.direction;
+
+        }
+        public override void FindFrame(int frameHeight)
+        {
+            if (NPC.velocity.X != 0 && NPC.velocity.Y == 0)
+            {
+                NPC.frameCounter += 0.1f;
+                NPC.frameCounter %= Main.npcFrameCount[NPC.type];
+                int frame = (int)NPC.frameCounter;
+                NPC.frame.Y = frame * frameHeight;
+            }
+            else
+            {
+                NPC.frame.Y = 0;
+            }
+        }
+
+        public override void SetBestiary(Terraria.GameContent.Bestiary.BestiaryDatabase database, Terraria.GameContent.Bestiary.BestiaryEntry bestiaryEntry)
+        {
+            bestiaryEntry.UIInfoProvider = new Terraria.GameContent.Bestiary.CommonEnemyUICollectionInfoProvider(ContentSamples.NpcBestiaryCreditIdsByNpcNetIds[Type], quickUnlock: true);
+            bestiaryEntry.Info.AddRange(new Terraria.GameContent.Bestiary.IBestiaryInfoElement[] {
+                new FlavorTextBestiaryInfoElement(CalRemixHelper.LocalText($"Bestiary.{Name}").Value)
+            });
+        }
+
+        public override void HitEffect(NPC.HitInfo hit)
+        {
+            for (int k = 0; k < 5; k++)
+            {
+                Dust.NewDust(NPC.position, NPC.width, NPC.height, DustID.GreenBlood, hit.HitDirection, -1f, 0, default, 1f);
+            }
+            if (NPC.life <= 0)
+            {
+                for (int k = 0; k < 20; k++)
+                {
+                    Dust.NewDust(NPC.position, NPC.width, NPC.height, DustID.GreenBlood, hit.HitDirection, -1f, 0, default, 1f);
+                }
+            }
+        }
+    }
+}
