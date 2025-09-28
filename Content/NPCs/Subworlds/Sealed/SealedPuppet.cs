2dc976a44ff9e3968c0c930081e4cd468e072d3b
GinYuH <72181395+GinYuH@users.noreply.github.com>
2025-09-28 16:02:43 -0700

    gang violence

--- Content/NPCs/Subworlds/Sealed/SealedPuppet.cs
+++ Content/NPCs/Subworlds/Sealed/SealedPuppet.cs
@@ -1,10 +1,15 @@
 ﻿using CalamityMod;
 using CalRemix.Core.Biomes;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Steamworks;
+using System.Collections.Generic;
 using Terraria;
 using Terraria.GameContent.Bestiary;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.ModLoader.Utilities;
+using Terraria.Utilities;
 using static Terraria.ModLoader.ModContent;
 //using CalamityMod.CalPlayer;
 
@@ -12,8 +17,18 @@ namespace CalRemix.Content.NPCs.Subworlds.Sealed
 {
     public class SealedPuppet : ModNPC
     {
+        public static List<int> infighting = new()
+        {
+            NPCType<SealedPuppet>(),
+            NPCType<SealedCitizen>(),
+            NPCType<EvilSealedPuppet>(),
+            NPCType<SealedPorswine>()
+        };
+
         public override void SetStaticDefaults()
         {
+            NPCID.Sets.TakesDamageFromHostilesWithoutBeingFriendly[Type] = true;
+            NPCID.Sets.UsesNewTargetting[Type] = true;
             Main.npcFrameCount[NPC.type] = 7;
         }
 
@@ -22,12 +37,14 @@ namespace CalRemix.Content.NPCs.Subworlds.Sealed
             NPC.CloneDefaults(NPCID.Squirrel);
             NPC.width = 30;
             NPC.height = 54;
-            AIType = NPCID.Squirrel;
+            //AIType = NPCID.Squirrel;
+            NPC.friendly = false;
             NPC.npcSlots = 1;
             NPC.HitSound = SoundID.NPCHit1;
             NPC.DeathSound = SoundID.NPCDeath1;
             NPC.lifeMax = 5000;
             NPC.defense = 10;
+            NPC.damage = 40;
             NPC.Calamity().VulnerableToHeat = true;
             NPC.Calamity().VulnerableToSickness = false;
             NPC.Calamity().VulnerableToCold = true;
@@ -36,14 +53,51 @@ namespace CalRemix.Content.NPCs.Subworlds.Sealed
 
         public override void AI()
         {
-            NPC.spriteDirection = NPC.direction;
+            NPCUtils.TargetSearchResults results = NPCUtils.SearchForTarget(NPC, NPCUtils.TargetSearchFlag.NPCs, npcFilter: (NPC n) => Collision.CanHitLine(NPC.Top, 1, 1, n.Center, 1, 1) && NPC.Distance(n.Center) < 920 && infighting.Contains(n.type) && n.type != Type);
+            if (results.NearestNPCIndex != -1)
+            {
+                NPC target = Main.npc[results.NearestNPCIndex];
+                NPC.aiStyle = -1;
+                int direction = NPC.Center.X < target.Center.X ? 1 : -1;
 
+                float acc = 0.1f;
+                float speed = 1.5f;
+
+                if (direction == 1 && NPC.velocity.X < speed)
+                {
+                    NPC.velocity.X += acc;
+                }
+                else if (direction == -1 && NPC.velocity.X > -speed)
+                {
+                    NPC.velocity.X -= acc;
+                }
+                NPC.direction = direction;
+
+                Vector2 frontTilePos = NPC.Bottom + new Vector2(NPC.direction * NPC.width / 2 + 1, -4);
+                Point tileCoords = frontTilePos.ToTileCoordinates();
+
+                Tile tile = Framing.GetTileSafely(tileCoords.X, tileCoords.Y);
+                Tile above = Framing.GetTileSafely(tileCoords.X, tileCoords.Y - 1);
+
+                if ((tile.HasTile && Main.tileSolid[tile.TileType] || Main.rand.NextBool(50)) && NPC.velocity.Y == 0)
+                {
+                    //if (!above.HasTile || !Main.tileSolid[above.TileType])
+                    {
+                        NPC.velocity.Y = -5f;
+                    }
+                }
+            }
+            else
+            {
+                NPC.aiStyle = NPCAIStyleID.Passive;
+            }
+            NPC.spriteDirection = NPC.direction;
         }
         public override void FindFrame(int frameHeight)
         {
             if (NPC.velocity.X != 0)
             {
-                NPC.frameCounter += 0.075f;
+                NPC.frameCounter += 0.1f;
                 NPC.frameCounter %= Main.npcFrameCount[NPC.type];
                 int frame = (int)NPC.frameCounter;
                 NPC.frame.Y = frame * frameHeight;
@@ -75,18 +129,38 @@ namespace CalRemix.Content.NPCs.Subworlds.Sealed
                 }
             }
         }
+
+        public override bool CanHitNPC(NPC target)
+        {
+            return infighting.Contains(target.type) && target.type != Type;
+        }
+
+        public override bool CanBeHitByNPC(NPC attacker)
+        {
+            return infighting.Contains(attacker.type) && attacker.type != Type;
+        }
+
+        public override void ModifyHitNPC(NPC target, ref NPC.HitModifiers modifiers)
+        {
+            modifiers.SourceDamage *= 50;
+        }
     }
 
     public class SealedCitizen : SealedPuppet
     {
-        public override void SetStaticDefaults()
-        {
-            Main.npcFrameCount[Type] = 1;
-        }
+        public override string Texture => "CalRemix/Content/NPCs/Subworlds/Sealed/SealedPuppet";
+
         public override void SetDefaults()
         {
             base.SetDefaults();
+            NPC.damage = 30;
             SpawnModBiomes = [GetInstance<TurnipBiome>().Type];
+        }
+
+        public override void PostDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+        {
+            Texture2D hat = Request<Texture2D>("CalRemix/Content/NPCs/Subworlds/Sealed/SealedCitizen").Value;
+            spriteBatch.Draw(hat, NPC.Top - screenPos + Vector2.UnitY * NPC.gfxOffY, null, NPC.GetAlpha(drawColor), NPC.rotation, hat.Size() / 2, NPC.scale, NPC.spriteDirection == -1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None, 0);
         }
     }
     public class EvilSealedPuppet : SealedPuppet
