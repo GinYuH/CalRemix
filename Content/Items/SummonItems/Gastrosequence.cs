6e369291458b5886e8b8fa483c337426fc2f5580
GinYuH <72181395+GinYuH@users.noreply.github.com>
2025-10-04 17:06:19 -0700

    start Ascended Monorian Gastropod

---
+++ Content/Items/SummonItems/Gastrosequence.cs
@@ -0,0 +1,53 @@
+﻿using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+using CalRemix.Content.NPCs.Bosses.Acideye;
+using CalamityMod;
+using CalamityMod.Items.Placeables;
+using CalRemix.Content.NPCs.Subworlds.Sealed;
+using CalRemix.Content.Items.Materials;
+using CalRemix.Content.Items.Placeables.Subworlds.Sealed;
+using CalRemix.Core.Biomes;
+
+namespace CalRemix.Content.Items.SummonItems
+{
+    public class Gastrosequence : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 20;
+            Item.maxStack = 1;
+            Item.value = 0;
+            Item.rare = ItemRarityID.Red;
+            Item.useAnimation = 30;
+            Item.useTime = 45;
+            Item.useStyle = ItemUseStyleID.HoldUp;
+            Item.consumable = false;
+        }
+        public override bool CanUseItem(Player player)
+        {
+            return !NPC.AnyNPCs(ModContent.NPCType<MonorianGastropodAscended>()) && player.InModBiome<VoidForestBiome>();
+        }
+        public override bool? UseItem(Player player)
+        {
+            if (player.whoAmI == Main.myPlayer)
+            {
+                SoundEngine.PlaySound(SoundID.Roar, player.position);
+                CalRemixHelper.SpawnNPCOnPlayer(player.whoAmI, ModContent.NPCType<MonorianGastropodAscended>());
+            }
+            return true;
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient<Gastrogel>(10)
+                .AddIngredient<GildedShard>(5)
+                .AddIngredient<LightResidue>(50)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
+        }
+    }
+}
--- Content/NPCs/Subworlds/Sealed/MonorianGastropodAscended.cs
+++ Content/NPCs/Subworlds/Sealed/MonorianGastropodAscended.cs
@@ -20,11 +20,14 @@ using CalamityMod.Particles;
 
 namespace CalRemix.Content.NPCs.Subworlds.Sealed
 {
-    public class MonorianGastropod : ModNPC
+    public class MonorianGastropodAscended : ModNPC
     {
         public Player Target => Main.player[NPC.target];
         public ref float Timer => ref NPC.ai[0];
         public ref float State => ref NPC.ai[1];
+
+        public ref float SlimeTimer => ref NPC.Calamity().newAI[1];
+
         public Vector2 Anchor
         {
             get => new Vector2(NPC.Calamity().newAI[2], NPC.Calamity().newAI[3]);
@@ -37,21 +40,28 @@ namespace CalRemix.Content.NPCs.Subworlds.Sealed
 
         public List<List<VerletSimulatedSegment>> Tentacles = new();
 
+        public override void SetStaticDefaults()
+        {
+            NPCID.Sets.ImmuneToAllBuffs[Type] = true;
+        }
+
         public override void SetDefaults()
         {
             NPC.aiStyle = -1;
             NPC.width = NPC.height = 40;
-            NPC.lifeMax = 12000;
-            NPC.damage = 100;
-            NPC.defense = 8;
+            NPC.lifeMax = 30000;
+            NPC.damage = 200;
+            NPC.defense = 26;
             NPC.knockBackResist = 0f;
             NPC.noGravity = true;
             NPC.HitSound = SoundID.NPCHit1;
             NPC.DeathSound = SoundID.NPCDeath1;
             NPC.noTileCollide = true;
-            NPC.value = Item.buyPrice(gold: 1);
-            NPC.rarity = 1;
+            NPC.value = Item.buyPrice(gold: 8);
+            NPC.boss = true;
+            NPC.chaseable = false;
             SpawnModBiomes = new int[1] { ModContent.GetInstance<VoidForestBiome>().Type };
+            Music = CalRemixMusic.TheCalamity;
         }
         public override void AI()
         {
@@ -76,50 +86,40 @@ namespace CalRemix.Content.NPCs.Subworlds.Sealed
                     NPC.netSpam = 0;
                 }
             NPC.TargetClosest();
-
             switch (State)
             {
                 case 0:
                     {
-                        Timer++;
-                        if (Timer % 150 == 0)
+                        NPC.chaseable = true;
+                        CalamityUtils.SmoothMovement(NPC, 20, Main.player[NPC.target].Center - NPC.Center, 7, 0.1f, true);
+
+                        if (SlimeTimer == 2)
                         {
-                            NPC.velocity = Vector2.UnitY.RotatedByRandom(MathHelper.TwoPi) * Main.rand.NextFloat(0.2f, 1.1f);
-                        }
-                        if (NPC.Center.Y < (Anchor.Y - 200) || NPC.Center.Y > (Anchor.Y + 200))
-                        {
-                            NPC.velocity.Y *= -1;
-                        }
-                        if (NPC.justHit && State == 0)
-                        {
-                            State = 1;
                             Timer = 0;
-                            NPC.noTileCollide = false;
+                            State = 2;
+                        }
+                        else if (Timer == (SlimeTimer > 2 ? 120 : 60))
+                        {
+                            Timer = 0;
+                            State = 1;
                         }
                     }
                     break;
                 case 1:
                     {
-                        NPC.chaseable = true;
-                        Timer++;
-                        CalamityUtils.SmoothMovement(NPC, 20, Main.player[NPC.target].Center - NPC.Center, 2, 0.1f, true);
-
-                        if (Timer == 60)
-                        {
-                            Timer = 0;
-                            State = 2;
-                        }
-                    }
-                    break;
-                case 2:
-                    {
-                        NPC.velocity *= 0.97f;
-                        Timer++;
+                        NPC.velocity *= 0.9f;
 
                         if (Timer < 90 && Timer % 5 == 0)
                         {
                             SoundEngine.PlaySound(SoundID.DD2_LightningBugZap with { PitchVariance = 1 }, NPC.Center);
                             GeneralParticleHandler.SpawnParticle(new PlasmaExplosion(NPC.Center - Vector2.UnitY * 500, Vector2.Zero, Color.Cyan, Vector2.One, Main.rand.NextFloat(), 0.05f, 0.1f, 20));
+                            int projCount = 8;
+                            for (int i = 0; i < projCount; i++)
+                            {
+                                GeneralParticleHandler.SpawnParticle(new PointParticle(NPC.Center - Vector2.UnitY * 500, Vector2.UnitY.RotatedBy(MathHelper.Lerp(-MathHelper.Pi, MathHelper.Pi, i / (float)projCount)) * 30, false, 25, 1f, Color.Teal));
+                                //Dust dust = Dust.NewDustPerfect(NPC.Center - Vector2.UnitY * 500, DustID.Vortex, Vector2.UnitY.RotatedBy(MathHelper.Lerp(-MathHelper.Pi, MathHelper.Pi, i / (float)projCount)) * 30, Scale: 4f);
+                                //dust.noGravity = true;
+                            }
                         }
 
                         if (Timer == 90)
@@ -127,22 +127,43 @@ namespace CalRemix.Content.NPCs.Subworlds.Sealed
                             SoundEngine.PlaySound(CommonCalamitySounds.LightningSound, NPC.Center);
                             if (Main.netMode != NetmodeID.MultiplayerClient)
                             {
-                                int projCount = 5;
+                                int projCount = 8;
                                 for (int i = 0; i < projCount; i++)
                                 {
-                                    Projectile.NewProjectile(NPC.GetSource_FromThis(), NPC.Center - Vector2.UnitY * 500, Vector2.UnitY.RotatedBy(MathHelper.Lerp(-MathHelper.PiOver4, MathHelper.PiOver4, i / (float)projCount)) * 10, ProjectileID.VortexLightning, CalRemixHelper.ProjectileDamage(120, 200), 1f, Main.myPlayer, Vector2.UnitY.RotatedBy(MathHelper.Lerp(-MathHelper.PiOver4, MathHelper.PiOver4, i / (float)projCount)).ToRotation(), Main.rand.Next(80));
+                                    Projectile.NewProjectile(NPC.GetSource_FromThis(), NPC.Center - Vector2.UnitY * 500, Vector2.UnitY.RotatedBy(MathHelper.Lerp(-MathHelper.Pi, MathHelper.Pi, i / (float)projCount)) * 10, ProjectileID.VortexLightning, CalRemixHelper.ProjectileDamage(120, 200), 1f, Main.myPlayer, Vector2.UnitY.RotatedBy(MathHelper.Lerp(-MathHelper.Pi, MathHelper.Pi, i / (float)projCount)).ToRotation(), Main.rand.Next(80));
                                 }
                             }
                         }
 
                         if (Timer >= 150)
                         {
-                            State = 1;
+                            State = 0;
+                            Timer = 0;
+                            SlimeTimer++;
+                        }
+                    }
+                    break;
+                case 2:
+                    {
+                        SlimeTimer = 22;
+                        NPC.velocity *= 0.97f;
+                        if (Timer == 120)
+                        {
+                            if (Main.netMode != NetmodeID.MultiplayerClient)
+                            {
+                                NPC.NewNPC(NPC.GetSource_FromThis(), (int)NPC.Center.X, (int)NPC.Center.Y - 1000, ModContent.NPCType<AuricSlime>());
+                            }
+                        }
+
+                        if (Timer > 180)
+                        {
+                            State = 0;
                             Timer = 0;
                         }
                     }
                     break;
             }
+            Timer++;
         }
 
         public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
@@ -157,7 +178,7 @@ namespace CalRemix.Content.NPCs.Subworlds.Sealed
                         if (i == 0)
                             continue;
                         VerletSimulatedSegment seg = Segments[i];
-                        Vector2 truePos = State == 2 ? seg.position + Main.rand.NextVector2Circular(4, 4) : seg.position;
+                        Vector2 truePos = State == 1 ? seg.position + Main.rand.NextVector2Circular(4, 4) : seg.position;
                         float dist = i > 0 ? Vector2.Distance(seg.position, Segments[i - 1].position) : 0;
                         if (dist <= 2)
                             dist = 2;
@@ -172,7 +193,7 @@ namespace CalRemix.Content.NPCs.Subworlds.Sealed
                         Main.EntitySpriteDraw(TextureAssets.MagicPixel.Value, truePos - Main.screenPosition, new Rectangle(0, 0, (int)dist, 2), c, rot, TextureAssets.BlackTile.Size() / 2, scalee, Microsoft.Xna.Framework.Graphics.SpriteEffects.None, 0);
                     }
                 }
-            float animSpeed = State == 2 ? 50 : 7;
+            float animSpeed = State == 1 ? 50 : 7;
             Vector2 scale = new Vector2(MathF.Sin(Main.GlobalTimeWrappedHourly * animSpeed) * 0.022f, MathF.Cos(Main.GlobalTimeWrappedHourly * animSpeed) * 0.022f);
             spriteBatch.Draw(texture, NPC.Center - Main.screenPosition + new Vector2(0f, NPC.gfxOffY), null, drawColor, NPC.rotation, texture.Size() / 2f, NPC.scale * Vector2.One + scale, 0, 0f);
             return false;
@@ -186,8 +207,23 @@ namespace CalRemix.Content.NPCs.Subworlds.Sealed
 
         public override void ModifyNPCLoot(NPCLoot npcLoot)
         {
-            npcLoot.Add(ModContent.ItemType<Gastrogel>(), 1, 15, 25);
-            npcLoot.Add(ModContent.ItemType<MonorianGemShards>(), 1, 6, 12);
+            npcLoot.Add(ModContent.ItemType<Gastrogel>(), 1, 35, 60);
+            npcLoot.Add(ModContent.ItemType<MonorianGemShards>(), 1, 12, 20);
+            npcLoot.Add(ModContent.ItemType<GastropodEye>());
+        }
+
+        public override bool? CanBeHitByItem(Player player, Item item)
+        {
+            return false;
+        }
+
+        public override bool? CanBeHitByProjectile(Projectile projectile)
+        {
+            if (projectile.type == ProjectileID.FallingStar)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
