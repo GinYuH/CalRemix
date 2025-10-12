e922d939b3eca42e49281e43d362ab562641da0d
GinYuH <72181395+GinYuH@users.noreply.github.com>
2025-10-11 21:31:14 -0700

    awoken gastropod eye

---
+++ Content/Projectiles/Weapons/AwokenGastropodEyeProj.cs
@@ -0,0 +1,28 @@
+﻿using CalamityMod.Particles;
+using CalamityMod.Projectiles.Rogue;
+using CalRemix.Content.NPCs.Subworlds.Sealed;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalRemix.Content.Projectiles.Weapons
+{
+    public class AwokenGastropodEyeProj : MinecraftArrow
+    {
+        public override string Texture => "CalRemix/Content/Items/Weapons/AwokenGastropodEye";
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.Magic;
+            Projectile.extraUpdates = 0;
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            target.AddBuff(BuffID.OnFire3, 180);
+        }
+    }
+}
