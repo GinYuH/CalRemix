06d1a5a92c02f723b2ebe9bed25da46b65ab68f4
GinYuH <72181395+GinYuH@users.noreply.github.com>
2025-10-27 12:11:07 -0700

    start Monorian Warrior

---
+++ Content/Projectiles/Hostile/ArkhalisProj.cs
@@ -0,0 +1,58 @@
+﻿using Terraria;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using System;
+using Terraria.ModLoader;
+using System.IO;
+
+namespace CalRemix.Content.Projectiles.Hostile
+{
+	public class ArkhalisProj : ModProjectile
+	{
+		private bool stopeh = false;
+		public override string Texture => $"Terraria/Images/Item_{ItemID.Arkhalis}";
+		
+		public override void SetDefaults()
+		{
+			Projectile.width = 20;
+			Projectile.height = 20;
+			Projectile.tileCollide = true;
+			Projectile.ignoreWater = false;
+			Projectile.hostile = true;
+			Projectile.aiStyle = 2;
+			Projectile.alpha = 150;
+			Projectile.timeLeft = 600;
+			AIType = ProjectileID.ThrowingKnife;
+		}
+		public override void AI()
+		{
+			if (Projectile.velocity.Y < 0) //Less than 0 means going up
+            {
+				Projectile.damage = 0;
+				Projectile.alpha -= 2;
+            }
+			else
+            {
+				Projectile.alpha = 0;
+				Projectile.damage = Main.expertMode ? 10 : 15;
+            }
+			if (Projectile.velocity.Y > 6 && !stopeh)
+            {
+				stopeh = true;
+				Projectile.velocity.Y = 6;
+            }
+			if (stopeh)
+            {
+				Projectile.velocity.Y += 0.02f;
+            }
+		}
+		public override void SendExtraAI(BinaryWriter writer)
+		{
+			writer.Write(stopeh);
+		}
+		public override void ReceiveExtraAI(BinaryReader reader)
+		{
+			stopeh = reader.ReadBoolean();
+		}
+	}
+}
---
+++ Content/Projectiles/Hostile/MonorianBolt.cs
@@ -0,0 +1,40 @@
+﻿using Terraria;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using System;
+using Terraria.ModLoader;
+
+namespace CalRemix.Content.Projectiles.Hostile
+{
+	public class MonorianBolt : ModProjectile
+	{
+		public override string Texture => "CalamityMod/Projectiles/InvisibleProj";
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 8;
+			Projectile.height = 8;
+			Projectile.hostile = true;
+			Projectile.ignoreWater = true;
+			Projectile.tileCollide = false;
+			Projectile.timeLeft = 180;
+			Projectile.aiStyle = ProjAIStyleID.GemStaffBolt;
+			AIType = ProjectileID.RubyBolt;
+		}
+		public override void AI()
+		{
+			/*Vector2 dustpos = Projectile.position;
+			for (int dusttimer = 0; dusttimer < 3; dusttimer++)
+			{
+				int dusty = Dust.NewDust(Projectile.position, 1, 1, DustID.GemRuby, 0f, 0f, 100, default);
+				Main.dust[dusty].noGravity = true;
+				Main.dust[dusty].noLight = true;
+			}
+			if (Main.rand.Next(8) == 0)
+            {
+				int dusto = Dust.NewDust(Projectile.position, 1, 1, DustID.GemRuby, 0f, 0f, 100, default);
+				Main.dust[dusto].noLight = true;
+			}*/
+		}
+	}
+}
