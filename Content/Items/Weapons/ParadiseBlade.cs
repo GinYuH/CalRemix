531848b319548ab156d78778c47201ceda4f4b8c
GinYuH <72181395+GinYuH@users.noreply.github.com>
2025-11-02 16:52:35 -0800

    added Paradise Blade

---
+++ Content/Items/Weapons/ParadiseBlade.cs
@@ -0,0 +1,73 @@
+﻿using CalamityMod.Particles;
+using CalRemix.Content.Items.Materials;
+using CalRemix.Content.Projectiles.Weapons;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalRemix.Content.Items.Weapons
+{
+    public class ParadiseBlade : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            ItemID.Sets.ItemsThatAllowRepeatedRightClick[Item.type] = true;
+        }
+        public override void SetDefaults()
+        {
+            Item.width = 70;
+            Item.damage = 3;
+            Item.DamageType = DamageClass.Generic;
+            Item.useAnimation = 28;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTime = 28;
+            Item.useTurn = true;
+            Item.knockBack = 2f;
+            Item.UseSound = BetterSoundID.ItemBell;
+            Item.autoReuse = true;
+            Item.height = 72;
+            Item.value = Item.buyPrice(100);
+            Item.rare = ItemRarityID.Cyan;
+        }
+
+        public override bool AltFunctionUse(Player player)
+        {
+            return true;
+        }
+
+        public override bool? UseItem(Player player)
+        {
+            if (player.altFunctionUse == 2)
+            {
+                SoundEngine.PlaySound(CalamityMod.Projectiles.Melee.PwnagehammerProj.UseSoundFunny with { Pitch = -1 }, player.Center);
+                Projectile.NewProjectile(Item.GetSource_FromThis(), Main.MouseWorld, Vector2.Zero, ModContent.ProjectileType<LightOrbGuiding>(), 0, 0, player.whoAmI);
+            }
+            return true;
+        }
+
+        public override void UseItemFrame(Player player)
+        {
+            if (player.ItemAnimationJustStarted)
+                player.itemLocation = (Vector2)player.HandPosition;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ModContent.ItemType<Mikado>())
+                .AddIngredient(ModContent.ItemType<GildedShard>(), 2)
+                .AddIngredient(ItemID.GoldWatch)
+                .AddTile(TileID.Anvils)
+                .Register();
+
+            CreateRecipe()
+                .AddIngredient(ModContent.ItemType<Mikado>())
+                .AddIngredient(ModContent.ItemType<GildedShard>(), 2)
+                .AddIngredient(ItemID.PlatinumWatch)
+                .AddTile(TileID.Anvils)
+                .Register();
+        }
+    }
+}
---
+++ Content/Projectiles/Weapons/LightOrbGuiding.cs
@@ -0,0 +1,44 @@
+﻿using CalamityMod;
+using CalamityMod.Particles;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalRemix.Content.Projectiles.Weapons
+{
+    public class LightOrbGuiding : ModProjectile
+    {
+        public override string Texture => "CalamityMod/Projectiles/InvisibleProj";
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 30;
+            Projectile.height = 30;
+            Projectile.friendly = true;
+            Projectile.timeLeft = 600;
+            Projectile.tileCollide = false;
+        }
+
+        public override void AI()
+        {
+            Lighting.AddLight(Projectile.Center, 4, 4, 0);
+            Vector2 partPos = Projectile.Center + Vector2.UnitY.RotatedByRandom(MathHelper.TwoPi) * 20;
+            GeneralParticleHandler.SpawnParticle(new SquishyLightParticle(partPos, partPos.DirectionTo(Projectile.Center) * 0.2f, Main.rand.NextFloat(0.8f, 1f), Color.Yellow, 16, 0.8f));
+
+            Main.player[Projectile.owner].buffImmune[BuffID.Darkness] = true;
+            Main.player[Projectile.owner].buffImmune[BuffID.Obstructed] = true;
+            Main.player[Projectile.owner].Calamity().externalAbyssLight += 50;
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            /*Texture2D texture = ModContent.Request<Texture2D>("CalamityMod/Particles/LargeBloom").Value;
+            Texture2D wing = ModContent.Request<Texture2D>("CalamityMod/Particles/LargeSpark").Value;
+            Vector2 centered = Projectile.Center - Main.screenPosition + new Vector2(0f, Projectile.gfxOffY);
+            Main.spriteBatch.Draw(texture, centered, null, Color.Yellow with { A = 0 }, Projectile.rotation, texture.Size() / 2, Projectile.scale * 0.2f, SpriteEffects.None, 0);
+            Main.spriteBatch.Draw(wing, centered, null, Color.Yellow with { A = 0 }, Projectile.rotation, wing.Size() / 2, new Vector2(0.5f, 1f) * Projectile.scale * 0.2f, SpriteEffects.None, 0);*/
+            return false;
+        }
+    }
+}
