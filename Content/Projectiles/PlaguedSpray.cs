b418ae35a2e8aa65e3ef985a2fc561b20f4c9245
GinYuH <72181395+GinYuH@users.noreply.github.com>
2025-11-17 21:01:58 -0800

    Plague ModBiomeConversion

--- Content/Projectiles/PlaguedSpray.cs
+++ Content/Projectiles/PlaguedSpray.cs
@@ -1,3 +1,4 @@
+using CalRemix.Content.Tiles.PlaguedJungle;
 using Microsoft.Xna.Framework;
 using System;
 using Terraria;
@@ -61,93 +62,13 @@ namespace CalRemix.Content.Projectiles
                 {
                     if (WorldGen.InWorld(k, l, 1) && Math.Abs(k - i) + Math.Abs(l - j) < Math.Sqrt(size * size + size * size))
                     {
-                        int type = Main.tile[k, l].TileType;
-                        int wall = Main.tile[k, l].WallType;
                         int dist = (k - i) * (k - i) + (l - j) * (l - j);
                         if (dist > size * size)
                             continue;
                         if (Main.tile[k, l] == null)
                             continue;
-                        if (type == TileID.Tin || type == TileID.Copper || type == TileID.Silver || type == TileID.Tungsten || type == TileID.Gold || type == TileID.Platinum || type == TileID.Iron || type == TileID.Lead || type == TileID.Demonite || type == TileID.Crimtane)
-                        {
-                            Main.tile[k, l].TileType = (ushort)ModContent.TileType<Tiles.PlaguedJungle.Sporezol>();
-                            NetMessage.SendTileSquare(-1, k, l, 1);
-                        }
-                        if (TileID.Sets.GrassSpecial[type] || type == TileID.Grass || type == TileID.CorruptGrass || type == TileID.CrimsonGrass)
-                        {
-                            Main.tile[k, l].TileType = (ushort)ModContent.TileType<Tiles.PlaguedJungle.PlaguedGrass>();
-                            NetMessage.SendTileSquare(-1, k, l, 1);
-                        }
-                        if (TileID.Sets.Mud[type])
-                        {
-                            Main.tile[k, l].TileType = (ushort)ModContent.TileType<Tiles.PlaguedJungle.PlaguedMud>();
-                            NetMessage.SendTileSquare(-1, k, l, 1);
-                        }
-                        if (TileID.Sets.Conversion.Stone[type])
-                        {
-                            Main.tile[k, l].TileType = (ushort)ModContent.TileType<Tiles.PlaguedJungle.PlaguedStone>();
-                            NetMessage.SendTileSquare(-1, k, l, 1);
-                        }
-                        if (wall == WallID.MudUnsafe || wall == WallID.DirtUnsafe || wall == WallID.DirtUnsafe2 || wall == WallID.DirtUnsafe3 || wall == WallID.DirtUnsafe4 || wall == WallID.Cave6Unsafe || wall == WallID.Cave5Unsafe || wall == WallID.Cave4Unsafe || wall == WallID.Cave3Unsafe || wall == WallID.Cave2Unsafe || wall == WallID.CaveUnsafe)
-                        {
-                            Main.tile[k, l].WallType = (ushort)ModContent.WallType<Tiles.PlaguedJungle.PlaguedMudWall>();
-                            NetMessage.SendTileSquare(-1, k, l, 1);
-                        }
-                        if (type == TileID.Hive)
-                        {
-                            Main.tile[k, l].TileType = (ushort)ModContent.TileType<Tiles.PlaguedJungle.PlaguedHive>();
-                            NetMessage.SendTileSquare(-1, k, l, 1);
-                        }
-                        if (type == TileID.Dirt)
-                        {
-                            Main.tile[k, l].TileType = (ushort)ModContent.TileType<Tiles.PlaguedJungle.PlaguedMud>();
-                            NetMessage.SendTileSquare(-1, k, l, 1);
-                        }
-                        if (type == TileID.RichMahogany)
-                        {
-                            Main.tile[k, l].TileType = (ushort)ModContent.TileType<Tiles.PlaguedJungle.PlaguedPipe>();
-                            NetMessage.SendTileSquare(-1, k, l, 1);
-                        }
-                        if (wall == WallID.RichMaogany)
-                        {
-                            Main.tile[k, l].WallType = (ushort)ModContent.WallType<Tiles.PlaguedJungle.PlaguedPipeWall>();
-                            NetMessage.SendTileSquare(-1, k, l, 1);
-                        }
-                        if (type == TileID.LivingMahogany || type == TileID.LivingWood)
-                        {
-                            Main.tile[k, l].TileType = (ushort)ModContent.TileType<Tiles.PlaguedJungle.UndeadPlaguePipe>();
-                            NetMessage.SendTileSquare(-1, k, l, 1);
-                        }
-                        if (wall == WallID.HiveUnsafe || wall == WallID.Hive || wall == WallID.GraniteUnsafe || wall == WallID.MarbleUnsafe || wall == WallID.Granite || wall == WallID.Marble)
-                        {
-                            Main.tile[k, l].WallType = (ushort)ModContent.WallType<Tiles.PlaguedJungle.PlaguedHiveWall>();
-                            NetMessage.SendTileSquare(-1, k, l, 1);
-                        }
-                        if (wall == WallID.Stone || wall == WallID.EbonstoneUnsafe || wall == WallID.CrimstoneUnsafe)
-                        {
-                            Main.tile[k, l].WallType = (ushort)ModContent.WallType<Tiles.PlaguedJungle.PlaguedStoneWall>();
-                            NetMessage.SendTileSquare(-1, k, l, 1);
-                        }
-                        if (wall == WallID.Grass || wall == WallID.GrassUnsafe || wall == WallID.CorruptGrassUnsafe || wall == WallID.CrimsonGrassUnsafe || wall == WallID.HallowedGrassUnsafe || wall == WallID.Jungle || wall == WallID.JungleUnsafe || wall == WallID.JungleUnsafe1 || wall == WallID.JungleUnsafe2 || wall == WallID.JungleUnsafe3 || wall == WallID.JungleUnsafe4 || wall == WallID.Flower || wall == WallID.FlowerUnsafe)
-                        {
-                            Main.tile[k, l].WallType = (ushort)ModContent.WallType<Tiles.PlaguedJungle.PlaguedVineWall>();
-                            NetMessage.SendTileSquare(-1, k, l, 1);
-                        }
-                        if (type == TileID.ClayBlock)
-                        {
-                            Main.tile[k, l].TileType = (ushort)ModContent.TileType<Tiles.PlaguedJungle.PlaguedClay>();
-                            NetMessage.SendTileSquare(-1, k, l, 1);
-                        }
-                        if (type == TileID.Silt)
-                        {
-                            Main.tile[k, l].TileType = (ushort)ModContent.TileType<Tiles.PlaguedJungle.PlaguedSilt>();
-                            NetMessage.SendTileSquare(-1, k, l, 1);
-                        }
-                        if (type == TileID.Sand)
-                        {
-                            Main.tile[k, l].TileType = (ushort)ModContent.TileType<Tiles.PlaguedJungle.PlaguedSand>();
-                            NetMessage.SendTileSquare(-1, k, l, 1);
-                        }
+
+                        WorldGen.Convert(k, l, ModContent.GetInstance<PlagueConversion>().Type, size);
                     }
                 }
             }
---
+++ Content/Tiles/PlaguedJungle/PlagueConversion.cs
@@ -0,0 +1,101 @@
+﻿using CalamityMod.Projectiles.Summon;
+using CalamityMod.Tiles.Astral;
+using CalamityMod.Tiles.AstralDesert;
+using CalamityMod.Walls;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalRemix.Content.Tiles.PlaguedJungle
+{
+    public class PlagueConversion : ModBiomeConversion
+    {
+        public override string Name => "Plague";
+
+        public override void PostSetupContent()
+        {
+            TileLoader.RegisterSimpleConversion(TileID.Grass, Type, ModContent.TileType<PlaguedGrass>());
+            TileLoader.RegisterSimpleConversion(TileID.JungleGrass, Type, ModContent.TileType<PlaguedGrass>());
+            TileLoader.RegisterSimpleConversion(TileID.Dirt, Type, ModContent.TileType<PlaguedMud>());
+            TileLoader.RegisterSimpleConversion(ModContent.TileType<AstralDirt>(), Type, ModContent.TileType<PlaguedClay>());
+            TileLoader.RegisterSimpleConversion(TileID.Mud, Type, ModContent.TileType<PlaguedMud>());
+            TileLoader.RegisterSimpleConversion(TileID.Stone, Type, ModContent.TileType<PlaguedStone>());
+            TileLoader.RegisterSimpleConversion(TileID.Silt, Type, ModContent.TileType<PlaguedSilt>());
+            TileLoader.RegisterSimpleConversion(ModContent.TileType<CelestialRemains>(), Type, ModContent.TileType<PlaguedClay>());
+            TileLoader.RegisterSimpleConversion(TileID.Sand, Type, ModContent.TileType<PlaguedSand>());
+            TileLoader.RegisterSimpleConversion(TileID.LivingWood, Type, ModContent.TileType<UndeadPlaguePipe>());
+            TileLoader.RegisterSimpleConversion(TileID.LivingMahogany, Type, ModContent.TileType<UndeadPlaguePipe>());
+            TileLoader.RegisterSimpleConversion(ModContent.TileType<AstralMonolith>(), Type, ModContent.TileType<PlaguedClay>());
+            TileLoader.RegisterSimpleConversion(TileID.Hive, Type, ModContent.TileType<PlaguedHive>());
+            TileLoader.RegisterSimpleConversion(TileID.Granite, Type, ModContent.TileType<PlaguedHive>());
+            TileLoader.RegisterSimpleConversion(TileID.Marble, Type, ModContent.TileType<PlaguedHive>());
+            TileLoader.RegisterSimpleConversion(TileID.ClayBlock, Type, ModContent.TileType<PlaguedClay>());
+            TileLoader.RegisterSimpleConversion(ModContent.TileType<AstralClay>(), Type, ModContent.TileType<PlaguedClay>());
+
+            TileLoader.RegisterSimpleConversion(TileID.Copper, Type, ModContent.TileType<Sporezol>());
+            TileLoader.RegisterSimpleConversion(TileID.Iron, Type, ModContent.TileType<Sporezol>());
+            TileLoader.RegisterSimpleConversion(TileID.Silver, Type, ModContent.TileType<Sporezol>());
+            TileLoader.RegisterSimpleConversion(TileID.Gold, Type, ModContent.TileType<Sporezol>());
+            TileLoader.RegisterSimpleConversion(TileID.Demonite, Type, ModContent.TileType<Sporezol>());
+            TileLoader.RegisterSimpleConversion(TileID.Tin, Type, ModContent.TileType<Sporezol>());
+            TileLoader.RegisterSimpleConversion(TileID.Lead, Type, ModContent.TileType<Sporezol>());
+            TileLoader.RegisterSimpleConversion(TileID.Tungsten, Type, ModContent.TileType<Sporezol>());
+            TileLoader.RegisterSimpleConversion(TileID.Platinum, Type, ModContent.TileType<Sporezol>());
+            TileLoader.RegisterSimpleConversion(TileID.Crimtane, Type, ModContent.TileType<Sporezol>());
+
+            WallLoader.RegisterSimpleConversion(WallID.DirtUnsafe, Type, ModContent.WallType<PlaguedMudWall>());
+            WallLoader.RegisterSimpleConversion(WallID.MudUnsafe, Type, ModContent.WallType<PlaguedMudWall>());
+            WallLoader.RegisterSimpleConversion(WallID.DirtUnsafe1, Type, ModContent.WallType<PlaguedMudWall>());
+            WallLoader.RegisterSimpleConversion(WallID.DirtUnsafe2, Type, ModContent.WallType<PlaguedMudWall>());
+            WallLoader.RegisterSimpleConversion(WallID.DirtUnsafe3, Type, ModContent.WallType<PlaguedMudWall>());
+            WallLoader.RegisterSimpleConversion(WallID.DirtUnsafe4, Type, ModContent.WallType<PlaguedMudWall>());
+            WallLoader.RegisterSimpleConversion(ModContent.WallType<AstralDirtWall>(), Type, ModContent.WallType<PlaguedVineWall>());
+            WallLoader.RegisterSimpleConversion(WallID.Stone, Type, ModContent.WallType<PlaguedStoneWall>());
+            WallLoader.RegisterSimpleConversion(WallID.CaveUnsafe, Type, ModContent.WallType<PlaguedStoneWall>());
+            WallLoader.RegisterSimpleConversion(WallID.Cave2Unsafe, Type, ModContent.WallType<PlaguedStoneWall>());
+            WallLoader.RegisterSimpleConversion(WallID.Cave3Unsafe, Type, ModContent.WallType<PlaguedStoneWall>());
+            WallLoader.RegisterSimpleConversion(WallID.Cave4Unsafe, Type, ModContent.WallType<PlaguedStoneWall>());
+            WallLoader.RegisterSimpleConversion(WallID.Cave5Unsafe, Type, ModContent.WallType<PlaguedStoneWall>());
+            WallLoader.RegisterSimpleConversion(WallID.Cave6Unsafe, Type, ModContent.WallType<PlaguedStoneWall>());
+            WallLoader.RegisterSimpleConversion(WallID.Cave7Unsafe, Type, ModContent.WallType<PlaguedStoneWall>());
+            WallLoader.RegisterSimpleConversion(WallID.Cave8Unsafe, Type, ModContent.WallType<PlaguedStoneWall>());
+            WallLoader.RegisterSimpleConversion(WallID.LivingWoodUnsafe, Type, ModContent.WallType<PlaguedPipeWall>());
+            WallLoader.RegisterSimpleConversion(ModContent.WallType<AstralMonolithWall>(), Type, ModContent.WallType<PlaguedVineWall>());
+            WallLoader.RegisterSimpleConversion(WallID.HiveUnsafe, Type, ModContent.WallType<PlaguedHiveWall>());
+            WallLoader.RegisterSimpleConversion(WallID.GraniteUnsafe, Type, ModContent.WallType<PlaguedHiveWall>());
+            WallLoader.RegisterSimpleConversion(WallID.MarbleUnsafe, Type, ModContent.WallType<PlaguedHiveWall>());
+            WallLoader.RegisterSimpleConversion(WallID.JungleUnsafe, Type, ModContent.WallType<PlaguedVineWall>());
+            WallLoader.RegisterSimpleConversion(WallID.GrassUnsafe, Type, ModContent.WallType<PlaguedVineWall>());
+            WallLoader.RegisterSimpleConversion(WallID.JungleUnsafe1, Type, ModContent.WallType<PlaguedVineWall>());
+            WallLoader.RegisterSimpleConversion(WallID.JungleUnsafe2, Type, ModContent.WallType<PlaguedVineWall>());
+            WallLoader.RegisterSimpleConversion(WallID.JungleUnsafe3, Type, ModContent.WallType<PlaguedVineWall>());
+            WallLoader.RegisterSimpleConversion(WallID.JungleUnsafe4, Type, ModContent.WallType<PlaguedVineWall>());
+            WallLoader.RegisterSimpleConversion(WallID.FlowerUnsafe, Type, ModContent.WallType<PlaguedVineWall>());
+
+            CommonTileConversion(ModContent.TileType<PlaguedClay>(), TileID.ClayBlock);
+            CommonTileConversion(ModContent.TileType<PlaguedMud>(), TileID.Mud);
+            CommonTileConversion(ModContent.TileType<PlaguedSilt>(), TileID.Silt);
+            CommonTileConversion(ModContent.TileType<PlaguedHive>(), TileID.Hive);
+            CommonTileConversion(ModContent.TileType<Sporezol>(), TileID.Copper);
+
+            CommmonWallConversion(ModContent.WallType<PlaguedMudWall>(), WallID.MudUnsafe);
+            CommmonWallConversion(ModContent.WallType<PlaguedPipeWall>(), WallID.RichMaogany);
+            CommmonWallConversion(ModContent.WallType<PlaguedHiveWall>(), WallID.HiveUnsafe);
+        }
+
+        public static void CommonTileConversion(int infectedTileID, ushort cleanTileID)
+        {
+            TileLoader.RegisterConversion(infectedTileID, BiomeConversionID.Crimson, cleanTileID);
+            TileLoader.RegisterConversion(infectedTileID, BiomeConversionID.Hallow, cleanTileID);
+            TileLoader.RegisterConversion(infectedTileID, BiomeConversionID.Corruption, cleanTileID);
+            TileLoader.RegisterConversion(infectedTileID, BiomeConversionID.Purity, cleanTileID);
+        }
+
+        public static void CommmonWallConversion(int infectedWallID, ushort cleanWallID)
+        {
+            WallLoader.RegisterConversion(infectedWallID, BiomeConversionID.Crimson, cleanWallID);
+            WallLoader.RegisterConversion(infectedWallID, BiomeConversionID.Hallow, cleanWallID);
+            WallLoader.RegisterConversion(infectedWallID, BiomeConversionID.Corruption, cleanWallID);
+            WallLoader.RegisterConversion(infectedWallID, BiomeConversionID.Purity, cleanWallID);
+        }
+    }
+}
