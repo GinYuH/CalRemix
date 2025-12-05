3c142e36a1485ed981fd4b3c8091ef973bd82e3f
Big E <evanmorrison42@gmail.com>
2025-12-05 14:26:51 -0500

    + Shader Progress

--- Assets/Effects/Ozma.fx
+++ Assets/Effects/Ozma.fx
@@ -1,32 +1,35 @@
 sampler baseTexture : register(s0);
 sampler noiseTexture : register(s1);
+sampler dissolveTexture : register(s2);
+sampler invertTexture : register(s3);
 
 float time : register(C0);
 float rotation;
-float2 zoom = (1,1);
+float dissolveRatio;
+float inversionRatio;
 
-// Marble appearance controls
-float blowUpPower = 2.5; // How spherical it looks
-float blowUpSize = 0.25; // Strength of the spherical distortion
-float noiseScale = 0.75; // Scale of the noise pattern
-float scrollSpeed = 0.005; // How fast the pattern flows
+float blowUpPower = 2.5; 
+float blowUpSize = 0.25; 
+float noiseScale = 0.75; 
+float scrollSpeed = 0.005;
 float noiseContrast = 1.75;
 float noiseRotation = 1.5708;
-float pixelation = 128; // Pixelation amount (0 = none, higher = more pixelated)
+float pixelation = 128; 
 
-// Expanded Ozma-inspired color palette
-float4 color1 = float4(1.0, 0.1, 0.1, 1.0); // red
-float4 color2 = float4(0.9, 0.9, 0.9, 1.0); // White/Light Gray
-float4 color3 = float4(0.8, 0.2, 1.0, 1.0); // Purple/Magenta
-float4 color4 = float4(0.4, 0.2, 0.8, 1.0); // Deep Purple
-float4 color5 = float4(0.2, 0.6, 1.0, 1.0); // Cyan/Blue
-float4 color6 = float4(0.9, 0.9, 0.9, 1.0); // White/Light Gray
-float4 color7 = float4(0.9, 0.9, 0.9, 1.0); // White/Light Gray
-float4 color8 = float4(1.0, 0.9, 0.2, 1.0); // Yellow
+float dissolveNoiseScale = 0.5;
+float invertNoiseScale = 1;
 
-float4 PixelShaderFunction(float2 coords : TEXCOORD0) : COLOR0
+float4 color1 = float4(1.0, 0.1, 0.1, 1.0); 
+float4 color2 = float4(0.9, 0.9, 0.9, 1.0); 
+float4 color3 = float4(0.8, 0.2, 1.0, 1.0); 
+float4 color4 = float4(0.4, 0.2, 0.8, 1.0); 
+float4 color5 = float4(0.2, 0.6, 1.0, 1.0);
+float4 color6 = float4(0.9, 0.9, 0.9, 1.0);
+float4 color7 = float4(0.9, 0.9, 0.9, 1.0); 
+float4 color8 = float4(1.0, 0.9, 0.2, 1.0); 
+
+float4 PixelShaderFunction(float2 coords : TEXCOORD0, float4 sampleColor : COLOR0) : COLOR0
 {
-    // Apply pixelation if enabled
     if (pixelation > 0.0)
     {
         float pixelSize = 1.0 / pixelation;
@@ -35,12 +38,12 @@ float4 PixelShaderFunction(float2 coords : TEXCOORD0) : COLOR0
     }
     
     float4 baseColor = tex2D(baseTexture, coords);
+
     if (!any(baseColor))
         return baseColor;
     
     float2 centeredCoords = coords - 0.5;
     
-    // Apply rotation
     float cosR = cos(rotation);
     float sinR = sin(rotation);
     float2 rotatedCoords = float2(
@@ -50,10 +53,8 @@ float4 PixelShaderFunction(float2 coords : TEXCOORD0) : COLOR0
     
     float2 uv = rotatedCoords;
     
-    // Distance from center for various effects
     float distanceFromCenter = length(uv - 0.5) * 2.0;
     
-    // Rotate UVs FIRST, before any distortion
     float cosNR = cos(noiseRotation);
     float sinNR = sin(noiseRotation);
     float2 centeredUV = uv - 0.5;
@@ -62,7 +63,6 @@ float4 PixelShaderFunction(float2 coords : TEXCOORD0) : COLOR0
         centeredUV.x * sinNR + centeredUV.y * cosNR
     ) + 0.5;
     
-    // === "Blow up" effect to make it look spherical ===
     float blownUpUVX = pow(abs(rotatedUV.x - 0.5) * 2.0, blowUpPower);
     float blownUpUVY = pow(abs(rotatedUV.y - 0.5) * 2.0, blowUpPower);
     
@@ -70,25 +70,21 @@ float4 PixelShaderFunction(float2 coords : TEXCOORD0) : COLOR0
         -blownUpUVY * blowUpSize * 0.5 + rotatedUV.x * (1.0 + blownUpUVY * blowUpSize),
         -blownUpUVX * blowUpSize * 0.5 + rotatedUV.y * (1.0 + blownUpUVX * blowUpSize)
     );
+        
+    blownUpUV.y += time * scrollSpeed;
     
-    // Scale the noise
+    float2 unscaledBlownUpCoords = blownUpUV;
+
+    if (tex2D(dissolveTexture, unscaledBlownUpCoords * dissolveNoiseScale).r <= dissolveRatio)
+        return (0, 0, 0, 0);
+    
     blownUpUV *= noiseScale;
     
-    // Scroll effect
-    blownUpUV.y = blownUpUV.y + time * scrollSpeed;
-    
-    // Sample noise for primary pattern
     float combinedNoise = tex2D(noiseTexture, blownUpUV).r;
-
     combinedNoise = ((combinedNoise - 0.5) * noiseContrast) + 0.5;
-    
-    // Add spherical shading (fresnel-like)
-    combinedNoise += pow(distanceFromCenter, 6.0) * 0.3;
-    
-    // Clamp noise to 0-1 range
+    combinedNoise += pow(distanceFromCenter, 6.0) * 0.3;    
     combinedNoise = saturate(combinedNoise);
     
-    // Map brightness to expanded gradient colors (8 colors instead of 4)
     float4 marbleColor;
     if (combinedNoise < 0.125)
         marbleColor = lerp(color1, color2, combinedNoise * 8.0);
@@ -107,17 +103,20 @@ float4 PixelShaderFunction(float2 coords : TEXCOORD0) : COLOR0
     else
         marbleColor = lerp(color8, color1, (combinedNoise - 0.875) * 8.0);
     
-    // Add rim highlight for glossy sphere look
     float rimLight = pow(1.0 - distanceFromCenter, 3.0);
     marbleColor.rgb -= rimLight * 0.25;
     
-    // Circular mask with harder edge for more solid appearance
+    if (tex2D(invertTexture, unscaledBlownUpCoords * invertNoiseScale).r < inversionRatio)
+        marbleColor.rgb = 1 - marbleColor.rgb;
+        
     float circleMask = 1.0 - smoothstep(0.95, 1.0, distanceFromCenter);
     
     marbleColor.a = baseColor.a * circleMask;
     
     if (marbleColor.a < 0.01)
         return float4(0, 0, 0, 0);
+    
+    marbleColor.rgb *= sampleColor.rgb;
     
     return marbleColor;
 }
--- Content/NPCs/Bosses/Darksol/Darksol.cs
+++ Content/NPCs/Bosses/Darksol/Darksol.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.Xna.Framework;
+﻿using CalamityMod;
+using CalamityMod.Projectiles.Boss;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.GameContent;
 using Terraria.Graphics.Shaders;
 using Terraria.ModLoader;
@@ -12,9 +17,56 @@ public class Darksol : ModNPC
 {
     public override string Texture => "CalRemix/Core/Graphics/Metaballs/BasicCircle";
 
-    private float time;
-    private float breakAmount;
-    private Vector3[] fragmentOffsets;
+    private float Time = 0;
+    private float AttackTime = 0;
+    private float BreakAmount = 0f;
+    private Player Target = null;
+
+    Vector2 orbiterBaseAngles = Vector2.Zero;
+
+    private enum DarksolAIState
+    {
+        TestAI,
+        //Phase 1
+        SpawnAnimation,
+        DarkSpears,
+        DarkEnergyTorrent,
+        GiantDarkEnergyOrb,
+        DarkEnergySpin,
+        SneakyCure, //When under 20,000 HP he has a chance to heal himself for 6,000 - 9,999 HP with his Cure move, the chances of him using it become higher the lower his HP is. Even if he uses this attack he still has a 50% chance of doing a regular attack immediately after it.
+        SpinDashes,
+        DarkMeteor,
+        DarkStormCall,
+        FleeAnimation,
+        //Phase 2
+        Phase2Transition,
+        TeleportDashes,
+        LaserBarrage,
+        DarkEnergyBurst,
+        TheTwelfthAttack,
+        FinalAttackInstaKiller, //When reduced to 0 HP he will unleash one final move that needs to be avoided in order to win.  If this move hits you even once you will die and he will despawn.
+        DeathAnimation
+    }
+    DarksolAIState AIState = DarksolAIState.SpawnAnimation;
+
+    private enum DarkOrbID
+    {
+        Standard = -1,
+        ArcLeft,
+        ArcRight,
+        FluctuateSpeed,
+        WiggleLeft,
+        WiggleRight,
+        FluctuateSpeedExtreme,
+        SplitShotMix,
+        SplitShot,
+        SplitShotDistanceSpread,
+        BackAndForth1Step,
+        BackAndForth2Step,
+        TurnAroundAtSomePoint,
+        TurnAround,
+        UpwardTurnAround
+    }
 
     public override void SetDefaults()
     {
@@ -22,7 +74,9 @@ public class Darksol : ModNPC
         NPC.height = 64;
         NPC.scale = 4f;
         NPC.damage = 20;
-        NPC.lifeMax = 1;
+        NPC.lifeMax = 55535;
+        NPC.LifeMaxNERB(55535, 55535, 55535); //Goozma is a Calamity boss that has 55,535 HP and can use the most powerful attacks out of any boss in the mod.
+        NPC.DR_NERD(0.9f, 0.9f, 0.9f, 0.9f); //Don't be fooled by his low HP as he takes reduced damage from all weapons.
         NPC.knockBackResist = 0f;
         NPC.noGravity = true;
         NPC.noTileCollide = true;
@@ -32,48 +86,124 @@ public class Darksol : ModNPC
         NPC.netAlways = true;
         //Music = -1;
         //SceneEffectPriority = SceneEffectPriority.None;
+    }
 
-        fragmentOffsets = new Vector3[5];
+    public override void OnSpawn(IEntitySource source)
+    {
+        GetTarget();
 
-        for (int i = 0; i < fragmentOffsets.Length; i++)
-        {
-            float angle = (i / 5f) * MathHelper.TwoPi;
-            fragmentOffsets[i] = new Vector3(
-                (float)Math.Cos(angle) * 0.5f,  // X direction
-                (float)Math.Sin(angle) * 0.5f,  // Y direction
-                i * 0.2f                         // Fade timing (0, 0.2, 0.4, 0.6, 0.8)
-            );
-        }
+        NPC.Center = Target.Center - Vector2.UnitY * 600f;
     }
 
     public override void AI()
     {
-        NPC.velocity = (Main.MouseWorld - NPC.Center) / 30f;
-        NPC.rotation *= 0.9f;
-        NPC.rotation += NPC.velocity.X / 128f;
-        time++;
+        Time++;
+
+        switch (AIState)
+        {
+            case DarksolAIState.TestAI:
+                NPC.velocity = (Main.MouseWorld - NPC.Center) / 30f;
+                NPC.rotation *= 0.9f;
+                NPC.rotation -= NPC.velocity.X / 128f;
+
+                GetTarget();
+                /*
+                if (AttackTime % 60 == 0)
+                {
+                    Vector2 dir = NPC.DirectionTo(Target.Center);
+                    float rot = dir.ToRotation();
+                    Projectile.NewProjectile(NPC.GetSource_FromThis(), NPC.Center, dir * 5f, ModContent.ProjectileType<DarkOrb>(), 100, 0f, ai0: (float)DarkOrbID.WiggleLeft, ai1: rot + MathHelper.PiOver2);
+
+                    Projectile.NewProjectile(NPC.GetSource_FromThis(), NPC.Center, dir * 5f, ModContent.ProjectileType<DarkOrb>(), 100, 0f, ai0: (float)DarkOrbID.WiggleRight, ai1: rot + MathHelper.PiOver2);
+                }
+                */
+                break;
+            case DarksolAIState.SpawnAnimation:
+                if (AttackTime <= 90f)
+                    NPC.Center = Target.Center - Vector2.Lerp(Vector2.UnitY * 600f, Vector2.UnitY * 256f, Time / 90f);
+                else
+                {
+                    PickAttack();
+                    return;
+                }
+                break;
+            case DarksolAIState.DarkSpears:
+                if(AttackTime < 90)
+                {
+                    if (AttackTime % 5 == 0)
+                        Projectile.NewProjectile(NPC.GetSource_FromThis(), NPC.Center, -12 * Vector2.UnitY.RotatedBy(Main.rand.NextFloat(-MathHelper.PiOver4, MathHelper.PiOver4)), ModContent.ProjectileType<HolySpear>(), 100, 1f, ai0: -2);
+                }
+                else if (AttackTime > 120 && AttackTime < 600)
+                {
+                    if (AttackTime % 10 == 0)
+                        Projectile.NewProjectile(NPC.GetSource_FromThis(), Target.Center + new Vector2(Main.rand.NextFloat(-800f, 800f), -600f), new Vector2(3, 3), ModContent.ProjectileType<HolySpear>(), 100, 1f);
+
+                    if (AttackTime % 30 == 0)
+                        Projectile.NewProjectile(NPC.GetSource_FromThis(), NPC.Center, NPC.DirectionTo(Target.Center) * 8f, ModContent.ProjectileType<HolySpear>(), 100, 1f);
+                }
+                else if(AttackTime >= 600)
+                {
+                    PickAttack();
+                    return;
+                }
+                break;
+            case DarksolAIState.DarkEnergyTorrent:
+                break;
+        }
+
+        AttackTime++;
+    }
+
+    private void GetTarget()
+    {
+        if (NPC.target < 0 || NPC.target == Main.maxPlayers || Main.player[NPC.target].dead || !Main.player[NPC.target].active)
+        {
+            // Below comment by Fabsol in regards to BoC. Likely would have used same logic for Goozma.
+            // Ignore tank players, target low HP players, Brain is smart
+            CalamityTargetingParameters options = CalamityTargetingParameters.BossDefaults;
+            options.aggroRatio = -1f;
+            options.finishThemOff = true;
+            CalamityUtils.CalamityTargeting(NPC, options);
+        }
+
+        Target = Main.player[NPC.target];
+    }
+
+    private void PickAttack()
+    {
+        //Need to figure out how to make Darksol Smart and ignore attacks the player is "Immune to"
+        //Only example of an attack immunity is the Miasma since supposedly Auric would have provided immunity to its debuffs.
+        AIState = DarksolAIState.TestAI;
+        AttackTime = 0;
     }
 
     public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
     {
         Texture2D bossTexture = TextureAssets.Npc[Type].Value;
+        Vector2 origin = bossTexture.Size() * 0.5f;
+        Vector2 stretchScale = Vector2.One + new Vector2((float)Math.Cos(Time / 15f) / 12f, (float)Math.Cos(Time / 15f) / -12f);
+        spriteBatch.Draw(bossTexture, NPC.Center - Main.screenPosition, null, Color.Black, 0f, origin, stretchScale * (NPC.scale - 0.15f), SpriteEffects.None, 0);
+
         var ozmaShader = GameShaders.Misc[$"{Mod.Name}:Ozma"];
 
+        BreakAmount = 0f;
+
         Vector2 worldPos = Vector2.Transform(NPC.Center - Main.screenPosition, Main.GameViewMatrix.ZoomMatrix) / Main.ScreenSize.ToVector2();
-        ozmaShader.Shader.Parameters["time"]?.SetValue(time);
+        ozmaShader.Shader.Parameters["time"]?.SetValue(Time);
+        ozmaShader.Shader.Parameters["dissolveRatio"]?.SetValue(BreakAmount);
+        ozmaShader.Shader.Parameters["inversionRatio"]?.SetValue((float)Math.Sin(Time / 60f) / 2f + 0.5f);
         ozmaShader.Shader.Parameters["worldPosition"]?.SetValue(NPC.Center);
         ozmaShader.Shader.Parameters["scale"]?.SetValue(new Vector2(bossTexture.Width, bossTexture.Height) * NPC.scale);
         ozmaShader.Shader.Parameters["rotation"]?.SetValue(NPC.rotation);
-        ozmaShader.Shader.Parameters["zoom"]?.SetValue(Main.GameViewMatrix.Zoom);
 
         spriteBatch.GraphicsDevice.Textures[1] = ModContent.Request<Texture2D>("CalamityMod/ExtraTextures/GreyscaleGradients/MeltyNoise").Value;
         spriteBatch.GraphicsDevice.SamplerStates[1] = SamplerState.LinearWrap;
 
-        // Optional: Customize colors (uncomment to change)
-        // ozmaShader.Parameters["color1"]?.SetValue(new Vector4(1.0f, 0.2f, 0.8f, 1.0f));
-        // ozmaShader.Parameters["color2"]?.SetValue(new Vector4(0.2f, 0.5f, 1.0f, 1.0f));
-        // ozmaShader.Parameters["color3"]?.SetValue(new Vector4(1.0f, 0.8f, 0.2f, 1.0f));
-        // ozmaShader.Parameters["color4"]?.SetValue(new Vector4(0.3f, 1.0f, 0.4f, 1.0f));
+        spriteBatch.GraphicsDevice.Textures[2] = ModContent.Request<Texture2D>("CalamityMod/ExtraTextures/GreyscaleGradients/Cracks").Value;
+        spriteBatch.GraphicsDevice.SamplerStates[2] = SamplerState.LinearWrap;
+
+        spriteBatch.GraphicsDevice.Textures[3] = ModContent.Request<Texture2D>("CalamityMod/ExtraTextures/GreyscaleGradients/Perlin").Value;
+        spriteBatch.GraphicsDevice.SamplerStates[3] = SamplerState.LinearWrap;
 
         // Apply the shader and draw
         spriteBatch.End();
@@ -88,8 +218,29 @@ public class Darksol : ModNPC
             Main.GameViewMatrix.TransformationMatrix
         );
 
-        Vector2 origin = new Vector2(bossTexture.Width / 2f, bossTexture.Height / 2f);
-        spriteBatch.Draw(bossTexture, NPC.Center - Main.screenPosition, null, Color.White, 0f, origin, NPC.scale, SpriteEffects.None, 0);
+        var Orbiters = GenerateRotatedRing(18, Quaternion.CreateFromYawPitchRoll(-Time / 100f, 0, Time / 100f));
+
+        List <Vector3> orbitersBehind = Orbiters.Where(v => v.Z < 0.5f).ToList();
+        orbitersBehind.Sort((a, b) => a.Z.CompareTo(b.Z));
+
+        List<Vector3> orbitersAhead = Orbiters.Where(v => v.Z >= 0.5f).ToList();
+        orbitersAhead.Sort((a, b) => a.Z.CompareTo(b.Z));
+
+        foreach (Vector3 v in orbitersBehind)
+        {
+            Vector2 posOffset = new Vector2(v.X, v.Y) * 200f;
+            float scaleMult = 1 + (v.Z / 2f);
+            spriteBatch.Draw(bossTexture, NPC.Center + posOffset - Main.screenPosition, null, Color.White.MultiplyRGB(Color.Lerp(Color.DarkGray, Color.White, v.Z / 2f + 0.5f)), 0f, origin, NPC.scale / 5f * scaleMult, SpriteEffects.None, 0);
+        }
+
+        spriteBatch.Draw(bossTexture, NPC.Center - Main.screenPosition, null, Color.Red, 0f, origin, stretchScale * NPC.scale, SpriteEffects.None, 0);
+
+        foreach (Vector3 v in orbitersAhead)
+        {
+            Vector2 posOffset = new Vector2(v.X, v.Y) * 200f;
+            float scaleMult = 1 + (v.Z / 2f);
+            spriteBatch.Draw(bossTexture, NPC.Center + posOffset - Main.screenPosition, null, Color.White.MultiplyRGB(Color.Lerp(Color.DarkGray, Color.White, v.Z / 2f + 0.5f)), 0f, origin, NPC.scale / 5f * scaleMult, SpriteEffects.None, 0);
+        }
 
         spriteBatch.End();
 
@@ -103,5 +254,32 @@ public class Darksol : ModNPC
             Main.GameViewMatrix.TransformationMatrix
         );
         return false;
+    }
+
+    public static List<Vector3> GenerateRotatedRing(int numPoints, Quaternion rotation)
+    {
+        List<Vector3> points = new List<Vector3>();
+        const float radius = 1f;
+
+        for (int i = 0; i < numPoints; i++)
+        {
+            float angle = 2f * (float)Math.PI * i / numPoints;
+
+            float x = (float)Math.Cos(angle) * radius;
+            float z = (float)Math.Sin(angle) * radius;
+            float y = 0f;
+
+            // Start with the local point in the XZ plane
+            Vector3 localPoint = new Vector3(x, y, z);
+
+            // Apply the desired rotation to the point
+            // The multiplication syntax depends on how your Quaternion struct operator is defined.
+            // Assuming the common syntax: result = rotation * vector
+            Vector3 rotatedPoint = Vector3.Transform(localPoint, rotation);
+
+            points.Add(rotatedPoint);
+        }
+
+        return points;
     }
 }
