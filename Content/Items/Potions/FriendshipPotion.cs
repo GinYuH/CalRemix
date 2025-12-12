2d013605989b8b8363677ca2a1e8a8c560b4ae8a
GinYuH <72181395+GinYuH@users.noreply.github.com>
2025-12-11 23:12:41 -0800

    more accurate recipe

--- Content/Items/Potions/FriendshipPotion.cs
+++ Content/Items/Potions/FriendshipPotion.cs
@@ -34,9 +34,11 @@ namespace CalRemix.Content.Items.Potions
         {
             CreateRecipe().
                 AddIngredient(ItemID.BottledWater).
-                AddIngredient(ModContent.ItemType<GalacticaSingularity>()).
-                AddIngredient(ModContent.ItemType<MeldBlob>(), 1).
-                AddTile(TileID.AlchemyTable).
+                AddIngredient(ModContent.ItemType<GalacticaSingularity>(). 10).
+                AddIngredient(ModContent.ItemType<MeldBlob>(), 10).
+                AddIngredient(ItemID.PixieDust, 20).
+                AddIngredient(ItemID.FallenStar, 20).
+                AddTile(TileID.Bottles).
                 Register();
         }
     }
