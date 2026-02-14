ded45362f6a7e829b6970caf964a04bd0e4fd78c
pil <harry.eglin@outlook.com>
2026-02-14 02:05:25 +0000

    accessory fusions

---
+++ Content/Items/Accessories/LuckyHorseshackle.cs
@@ -0,0 +1,34 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using CalamityMod.Items;
+
+namespace CalRemix.Content.Items.Accessories
+{
+    public class LuckyHorseshackle : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 28;
+            Item.height = 32;
+            Item.accessory = true;
+            Item.defense = 1;
+            Item.rare = ItemRarityID.Blue;
+            Item.value = CalamityGlobalItem.RarityBlueBuyPrice;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.noFallDmg = true;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe().
+                AddIngredient(ItemID.Shackle, 1).
+                AddIngredient(ItemID.LuckyHorseshoe, 1).
+                AddTile(TileID.TinkerersWorkbench).
+            Register();
+        }
+    }
+}
