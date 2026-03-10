00c4a8ab9504a799b974ea412885fc7804c41520
GinYuH <72181395+GinYuH@users.noreply.github.com>
2026-03-10 00:01:06 -0700

    car headlights and draw order

--- Content/NPCs/Subworlds/Car.cs
+++ Content/NPCs/Subworlds/Car.cs
@@ -1,12 +1,17 @@
 ﻿using CalamityMod;
+using CalamityMod.Graphics.Primitives;
 using CalamityMod.Sounds;
+using CalRemix.Content.Items.Accessories;
+using CalRemix.Core.Subworlds;
 using CalRemix.Core.World;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using SubworldLibrary;
 using System;
+using System.Collections.Generic;
 using Terraria;
 using Terraria.Audio;
+using Terraria.DataStructures;
 using Terraria.GameContent;
 using Terraria.GameContent.Bestiary;
 using Terraria.ID;
@@ -75,10 +80,13 @@ namespace CalRemix.Content.NPCs.Subworlds
                 {
                     NPC.velocity.X = 0;
                 }
-                person.Center = NPC.Center + new Vector2(0, -20) + NPC.velocity;
+                person.Center = NPC.Center + new Vector2(20, -20) + NPC.velocity;
                 person.velocity = Vector2.Zero;
                 person.direction = 1;
+                person.bodyFrame.Y = person.bodyFrame.Height * 3;
                 NPC.rotation += NPC.velocity.X * 0.03f;
+
+                Lighting.AddLight(NPC.Left + Vector2.UnitX * 200, 1f, 1f, 0.8f);
 
                 if (!SubworldSystem.AnyActive() && person.controlUseItem)
                 {
@@ -107,7 +115,53 @@ namespace CalRemix.Content.NPCs.Subworlds
             spriteBatch.Draw(wheel, NPC.Bottom - screenPos + new Vector2(-60, -10 - offY), null, NPC.GetAlpha(drawColor), wheelRotation, new Vector2(wheel.Width / 2, wheel.Height / 2), NPC.scale, 0, 0);
             spriteBatch.Draw(wheel, NPC.Bottom - screenPos + new Vector2(80, -10 - offY), null, NPC.GetAlpha(drawColor), wheelRotation, new Vector2(wheel.Width / 2, wheel.Height / 2), NPC.scale, 0, 0);
 
+            if (NPC.ai[0] == 1)
+            {
+                List<Vector2> points = new List<Vector2>();
+                Vector2 start = NPC.Left + Vector2.UnitX * 160;
+                for (int i = 0; i < 30; i++)
+                {
+                    points.Add(Vector2.Lerp(start, start + Vector2.UnitX * Main.screenWidth / 2, i / 29f));
+                }
+
+                PrimitiveRenderer.RenderTrail(points, new PrimitiveSettings(new PrimitiveSettings.VertexWidthFunction((float f, Vector2 v) => MathHelper.Lerp(4, 50, f)), new((float f, Vector2 v) => Color.Lerp(Color.Tan * 0.5f, default, CalamityUtils.SineOutEasing(f, 1)) * (1 + MathF.Sin(Main.GlobalTimeWrappedHourly * 5) * 0.15f))));
+            }
+
             return false;
+        }
+    }
+    public class CarDrawLayer : PlayerDrawLayer
+    {
+        public override bool GetDefaultVisibility(PlayerDrawSet drawInfo)
+        {
+            if (!SubworldSystem.IsActive<NightlineSubworld>())
+            {
+                return false;
+            }
+            return NPC.AnyNPCs(ModContent.NPCType<Car>());
+        }
+        public override Position GetDefaultPosition() => new AfterParent(PlayerDrawLayers.LastVanillaLayer);
+        protected override void Draw(ref PlayerDrawSet drawInfo)
+        {
+            if (drawInfo.shadow != 0f)
+                return;
+
+            foreach (NPC n in Main.ActiveNPCs)
+            {
+                if (n.type == ModContent.NPCType<Car>())
+                {
+                    Texture2D tex = TextureAssets.Npc[n.type].Value;
+                    Texture2D wheel = ModContent.Request<Texture2D>("CalRemix/Content/NPCs/Subworlds/Car_Wheel").Value;
+                    float offY = 8;
+                    Color lightColor = n.GetAlpha(Lighting.GetColor(n.Center.ToTileCoordinates()));
+                    drawInfo.DrawDataCache.Add(new DrawData(tex, n.Bottom - Main.screenPosition - Vector2.UnitY * (offY + 2 * MathF.Sin(n.localAI[0])), null, lightColor, 0, new Vector2(tex.Width / 2, tex.Height), n.scale, 0, 0));
+                    float wheelRotation = n.rotation;
+                    drawInfo.DrawDataCache.Add(new DrawData(wheel, n.Bottom - Main.screenPosition + new Vector2(-60, -10 - offY), null, lightColor, wheelRotation, new Vector2(wheel.Width / 2, wheel.Height / 2), n.scale, 0, 0));
+                    drawInfo.DrawDataCache.Add(new DrawData(wheel, n.Bottom - Main.screenPosition + new Vector2(80, -10 - offY), null, lightColor, wheelRotation, new Vector2(wheel.Width / 2, wheel.Height / 2), n.scale, 0, 0));
+
+                    return;
+                }
+            }
         }
     }
 }
