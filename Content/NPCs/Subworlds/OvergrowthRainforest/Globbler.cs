f678e972d744c64ad2f59800b027120fcbcec6e8
GinYuH <72181395+GinYuH@users.noreply.github.com>
2026-04-06 00:11:43 -0700

    start Globbler

---
+++ Content/NPCs/Subworlds/OvergrowthRainforest/Globbler.cs
@@ -0,0 +1,160 @@
+﻿using CalamityMod;
+using CalamityMod.BiomeManagers;
+using CalamityMod.DataStructures;
+using CalamityMod.Graphics.Primitives;
+using CalamityMod.Items.Materials;
+using CalamityMod.NPCs.Abyss;
+using CalamityMod.NPCs.NormalNPCs;
+using CalamityMod.Sounds;
+using CalRemix.Content.Items.Placeables.Banners;
+using CalRemix.Content.Items.Placeables.Subworlds.Wolf;
+using CalRemix.Core.Biomes.Subworlds;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using Terraria;
+using Terraria.Audio;
+using Terraria.Enums;
+using Terraria.GameContent;
+using Terraria.GameContent.Bestiary;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Utilities;
+using XPT.Core.Audio.MP3Sharp.Decoding;
+
+namespace CalRemix.Content.NPCs.Subworlds.OvergrowthRainforest
+{
+    public class Globbler : ModNPC
+    {
+        public NPC[] Arms = new NPC[2];
+
+        public override void SetDefaults()
+        {
+            NPC.aiStyle = -1;
+            NPC.damage = 10;
+            NPC.width = 40;
+            NPC.height = 40;
+            NPC.defense = 5;
+            NPC.knockBackResist = 0.6f;
+            NPC.lifeMax = 2000;
+            NPC.value = Item.buyPrice(silver: 1);
+            NPC.HitSound = SoundID.NPCHit25;
+            NPC.DeathSound = SoundID.NPCDeath28;
+            NPC.noGravity = true;
+            NPC.Calamity().VulnerableToHeat = true;
+            NPC.Calamity().VulnerableToCold = true;
+            NPC.Calamity().VulnerableToSickness = true;
+            SpawnModBiomes = [ModContent.GetInstance<OvergrowthRainforestBiome>().Type];
+        }
+
+        public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+        {
+            bestiaryEntry.Info.AddRange(new IBestiaryInfoElement[] {
+        new FlavorTextBestiaryInfoElement(CalRemixHelper.LocalText($"Bestiary.{Name}").Value)
+            });
+        }
+
+        public override void AI()
+        {
+            NPC.velocity = Vector2.Zero;
+            for (int i = 0; i < Arms.Length; i++)
+            {
+                NPC n = Arms[i];
+                if (n == null || !n.active || n.type != ModContent.NPCType<Globbler_Arm>())
+                {
+                    Arms[i] = Main.npc[NPC.NewNPC(NPC.GetSource_FromThis(), (int)NPC.Center.X, (int)NPC.Center.Y, ModContent.NPCType<Globbler_Arm>(), ai0: NPC.whoAmI)];
+                }
+            }
+
+            if (NPC.ai[2] == 0)
+            {
+                NPC.ai[2] = 1;
+                NPC.velocity.X = Main.rand.NextBool().ToDirectionInt();
+            }
+            else
+            {
+                if (NPC.ai[3] <= 0)
+                {
+                    int choice = Main.rand.Next(0, 2);
+                    NPC randomArm = Arms[choice];
+                    if (randomArm != null)
+                    if (randomArm.ModNPC != null)
+                    {
+                        if (randomArm.ModNPC<Globbler_Arm>() != null)
+                        {
+                            Globbler_Arm army = randomArm.ModNPC<Globbler_Arm>();
+
+                            army.Launch();
+                            NPC.ai[3] = 30;
+                            NPC.localAI[0] = choice;
+                        }
+                    }
+                }
+                for (int i = 0; i < Arms.Length; i++)
+                {
+                    NPC queriedArm = Arms[i];
+                    if (queriedArm != null)
+                    if (queriedArm.ModNPC != null)
+                    {
+                        if (queriedArm.ModNPC<Globbler_Arm>() != null)
+                        {
+                            Globbler_Arm army = queriedArm.ModNPC<Globbler_Arm>();
+
+                            if (army.Swinging)
+                            {
+                                NPC.Center = army.segments[0].position;
+                            }
+                        }
+                    }
+                }
+            }
+            NPC.ai[3]--;
+
+            int choicer = (int)NPC.localAI[0];
+            if (Arms[choicer] != null && Arms[choicer].active && Arms[choicer].type == ModContent.NPCType<Globbler_Arm>())
+            {
+                NPC npcArm = Arms[choicer];
+                Globbler_Arm globArm = npcArm.ModNPC<Globbler_Arm>();
+                if (globArm.Latched)
+                {
+                    int other = (choicer == 1) ? 0 : 1;
+                    Arms[other].ModNPC<Globbler_Arm>().Release();
+                }
+            }
+
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+        {
+            Texture2D tex = TextureAssets.Npc[Type].Value;
+
+            Vector2 drawPos = NPC.Bottom - screenPos;
+
+            spriteBatch.Draw(tex, NPC.Center - screenPos, null, NPC.GetAlpha(drawColor), NPC.rotation, new Vector2(tex.Width / 2, tex.Height / 2), NPC.scale, 0, 0);
+            return false;
+        }
+
+        public override void HitEffect(NPC.HitInfo hit)
+        {
+            for (int k = 0; k < 5; k++)
+            {
+                Dust.NewDust(NPC.position, NPC.width, NPC.height, DustID.GreenBlood, hit.HitDirection, -1f, 0, default, 1f);
+            }
+            if (NPC.life <= 0)
+            {
+                for (int k = 0; k < 20; k++)
+                {
+                    Dust.NewDust(NPC.position, NPC.width, NPC.height, DustID.GreenBlood, hit.HitDirection, -1f, 0, default, 1f);
+                }
+            }
+        }
+
+        public override void ModifyNPCLoot(NPCLoot npcLoot)
+        {
+            npcLoot.Add(ItemID.AcidDye, 1, 8, 13);
+        }
+    }
+}
---
+++ Content/NPCs/Subworlds/OvergrowthRainforest/Globbler_Arm.cs
@@ -0,0 +1,154 @@
+﻿using CalamityMod;
+using CalamityMod.DataStructures;
+using CalamityMod.Graphics.Primitives;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using System.IO;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalRemix.Content.NPCs.Subworlds.OvergrowthRainforest
+{
+    public class Globbler_Arm : ModNPC
+    {
+        public NPC dad => Main.npc[(int)NPC.ai[0]];
+        public Vector2 dest
+        {
+            get => new Vector2(NPC.ai[1], NPC.ai[2]);
+            set
+            { 
+                NPC.ai[1] = value.X;
+                NPC.ai[2] = value.Y;
+            }
+        }
+
+        public bool Swinging = false;
+        public bool Latched => NPC.ai[3] == 2;
+
+        public List<VerletSimulatedSegment> segments;
+        public override void SetStaticDefaults()
+        {
+            this.HideFromBestiary();
+        }
+        public override void SetDefaults()
+        {
+            NPC.dontTakeDamage = true;
+            NPC.aiStyle = -1;
+            NPC.damage = 0;
+            NPC.width = 10;
+            NPC.height = 10;
+            NPC.defense = 0;
+            NPC.knockBackResist = 0;
+            NPC.lifeMax = 5;
+            NPC.value = 0;
+            NPC.HitSound = SoundID.NPCHit25;
+            NPC.DeathSound = SoundID.NPCDeath28;
+            NPC.noTileCollide = true;
+            NPC.noGravity = true;
+            NPC.Calamity().VulnerableToHeat = true;
+            NPC.Calamity().VulnerableToCold = true;
+            NPC.Calamity().VulnerableToSickness = true;
+        }
+
+        public override void AI()
+        {
+            if (!dad.active || dad.type != ModContent.NPCType<Globbler>())
+            {
+                NPC.active = false;
+                return;
+            }
+            CalRemixHelper.CreateVerletChain(ref segments, 20, dad.Center, [0]);
+
+            if (NPC.ai[3] == 1)
+            {
+                if (NPC.Center.Y < (dad.Center.Y - 16) && Collision.SolidTiles(NPC.position, NPC.width, NPC.height))
+                {
+                    NPC.velocity = Vector2.Zero;
+                    NPC.ai[3] = 2;
+                }
+                if (NPC.localAI[2] <= 0)
+                {
+                    NPC.ai[3] = 0;
+                    Swinging = false;
+                }
+            }
+            if (Latched)
+            {
+                NPC.velocity = Vector2.Zero;
+            }
+
+            if (segments.Count > 0)
+            {
+                if (Swinging)
+                {
+                    segments[0].locked = false;
+                    segments[^1].locked = true;
+                    segments[^1].oldPosition = segments[^1].position;
+                    segments[^1].position = NPC.Center;
+                }
+                else
+                {
+                    segments[0].locked = true;
+                    segments[0].oldPosition = segments[0].position;
+                    segments[0].position = dad.Center;
+                    segments[^1].locked = false;
+                    if (Latched)
+                    NPC.Center = segments[^1].position;
+                }
+
+                VerletSimulatedSegment.SimpleSimulation(segments, 10, 30, 1);
+            }
+            NPC.localAI[2]--;
+        }
+
+        public void Launch()
+        {
+            int dir = dad.velocity.X.DirectionalSign();
+            if (Main.rand.NextBool(5))
+                dir *= -1;
+
+            NPC.velocity = -Vector2.UnitY.RotatedByRandom(MathHelper.PiOver4) * 10;
+            NPC.ai[3] = 1;
+            NPC.localAI[2] = 90;
+            Swinging = true;
+        }
+
+        public void Release()
+        {
+            NPC.ai[3] = 0;
+            Swinging = false;
+        }
+
+        public override void SendExtraAI(BinaryWriter writer)
+        {
+            writer.Write(Swinging);
+        }
+
+        public override void ReceiveExtraAI(BinaryReader reader)
+        {
+            Swinging = reader.ReadBoolean();
+        }
+
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+        {
+            spriteBatch.ExitShaderRegion();
+            if (segments.Count > 0)
+            {
+                List<Vector2> pts = new();
+                for (int i = 0; i < segments.Count; i++)
+                {
+                    pts.Add(segments[i].position);
+                }
+                PrimitiveRenderer.RenderTrail(pts, new PrimitiveSettings(new PrimitiveSettings.VertexWidthFunction((float f, Vector2 v) => 4), new PrimitiveSettings.VertexColorFunction((float f, Vector2 v) => Color.SeaGreen)));
+
+                Texture2D arm = ModContent.Request<Texture2D>(Texture).Value;
+                spriteBatch.Draw(arm, segments[^1].position - Main.screenPosition, null, Lighting.GetColor(segments[^1].position.ToTileCoordinates()), segments[^1].position.DirectionTo(dad.Center).ToRotation(), arm.Size() / 2, NPC.scale, 0, 0);
+            }
+            return false;
+        }
+    }
+}
