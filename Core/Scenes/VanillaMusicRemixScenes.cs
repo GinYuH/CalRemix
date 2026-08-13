a1652ee0e281fcfc04cbb60f7ca4dbed9cd17c5e
TohruKobayashi <t0hruk0b4y4sh1@gmail.com>
2026-08-13 16:24:23 -0500

    oops

--- Core/Scenes/VanillaMusicRemixScenes.cs
+++ Core/Scenes/VanillaMusicRemixScenes.cs
@@ -58,7 +58,7 @@ namespace CalRemix.Core.Scenes
             {
                 foreach (NPC npc in Main.npc)
                 {
-                    if (npc.type == NPCID.QueenSlimeBoss)
+                    if (npc.active == true && npc.type == NPCID.QueenSlimeBoss)
                     {
                         return true;
                     }
@@ -100,7 +100,7 @@ namespace CalRemix.Core.Scenes
         {
             foreach (NPC npc in Main.npc)
             {
-                if (npc.type == NPCID.GoblinPeon || npc.type == NPCID.GoblinThief || npc.type == NPCID.GoblinWarrior || npc.type == NPCID.GoblinArcher || npc.type == NPCID.GoblinSummoner)
+                if (npc.active == true && npc.type == NPCID.GoblinPeon || npc.type == NPCID.GoblinThief || npc.type == NPCID.GoblinWarrior || npc.type == NPCID.GoblinArcher || npc.type == NPCID.GoblinSummoner)
                 {
                     return true;
                 }
