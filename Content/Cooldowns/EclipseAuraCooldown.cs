cde850f5706d572add3eda1b2f862527086228c0
GinYuH <dodosucc@yahoo.com>
2026-08-14 23:26:55 -0700

    eclipse cooldown asset fix

--- Content/Cooldowns/EclipseAuraCooldown.cs
+++ Content/Cooldowns/EclipseAuraCooldown.cs
@@ -9,7 +9,7 @@ namespace CalRemix.Content.Cooldowns
         public static new string ID => "EclipseAura";
         public override bool ShouldDisplay => true;
         public override LocalizedText DisplayName => Language.GetOrRegister("Eclipse Aura Cooldown");
-        public override string Texture => "CalamityMod/Content/Cooldowns/EclipseEvade";
+        public override string Texture => "CalamityMod/Cooldowns/EclipseEvade";
         public override Color OutlineColor => new Color(152, 206, 248);
         public override Color CooldownStartColor => new Color(255, 192, 71);
         public override Color CooldownEndColor => new Color(255, 255, 151);
